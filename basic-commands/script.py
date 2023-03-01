178b0e0f41de21bf01a9d5c70ec3a2a6434d9b62
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-01 16:12:38 -0300

    criando scripts e aprendendo a usar variaveis

---
+++ basic-commands/script.py
@@ -0,0 +1,6 @@
+nome = input('Qual é o seu nome? ')
+idade = input('Qual a sua idade? ')
+peso = input('Quanto você pesa? ')
+pais = input('De que país você é? ')
+
+print (nome, idade, peso, pais)
--- basic-commands/variaveis.py
+++
@@ -1,5 +0,0 @@
-nome=input('Qual é seu nome?')
-idade=input('Digite a sua idade')
-peso=input('Digite o seu peso')
-
-print(nome,idade,peso)
