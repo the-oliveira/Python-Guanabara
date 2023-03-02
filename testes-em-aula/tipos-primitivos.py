bf8b5dbefdd23be084abcb6c31c23f4faef27019
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-02 18:19:04 -0300

    aprendendo tipos primitivos

---
+++ testes-em-aula/tipos-primitivos.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+s = int(n1) + int(n2)
+
+#print('O valor de', n1, '+', n2, 'é igual a: {}'.format(s))
+
+print('O valor de {} + {} é igual a: {}.'.format(n1, n2, s))
+
