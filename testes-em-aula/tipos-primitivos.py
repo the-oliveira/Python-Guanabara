048fac55a701146a8ec39f8c6764e5cb12d48747
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-02 18:27:14 -0300

    praticando

--- testes-em-aula/tipos-primitivos.py
+++ testes-em-aula/tipos-primitivos.py
@@ -2,7 +2,7 @@ n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 s = int(n1) + int(n2)
 
-#print('O valor de', n1, '+', n2, 'é igual a: {}'.format(s))
+#print('O valor de', n1, '+', n2, 'é igual a: {}'.format(s)) << Formato antigo
 
 print('O valor de {} + {} é igual a: {}.'.format(n1, n2, s))
 
