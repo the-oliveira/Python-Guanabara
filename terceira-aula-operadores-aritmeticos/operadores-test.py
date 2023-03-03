583dff290704515e8fd8d5ce09959197726b0cb7
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-03 11:29:17 -0300

    anotações importantes

---
+++ terceira-aula-operadores-aritmeticos/operadores-test.py
@@ -0,0 +1,14 @@
+#Operadores aritiméticos:
+#Adição: +
+#Subtração: -
+#Multiplicação: *
+#Divisão: /
+#Potência: **
+#Divisão inteira: //
+#Resto da divisão: %
+
+#Ordem de precedência(ordem de execução)
+#Primeiro: () Parenteses
+#Segundo: ** Potências
+#Terceiro: * / // e % (Multiplicação, divisão, divisão inteira e resto, executa quem vier primeiro)
+#Quarto: + e - (Soma e Subtração)
