17eadfbb394b9dad2d119944017f14d30bfa1e11
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-03 12:06:51 -0300

    Create ex006-operadores-aritm.py

---
+++ Exercicios/ex006-operadores-aritm.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor: '))
+
+print('O valor digitado foi {}\nO dobro de {} é {}\nO triplo de {} é {}\nA raiz quadrada de {} é {}'.format(n1,n1, n1*2, n1, n1*3, n1, n1**(1/2)))
+
