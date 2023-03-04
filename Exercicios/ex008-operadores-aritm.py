09e105b9eb2f0b3af38dd8cf6c82e84bfe838d84
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 12:46:58 -0300

    Update ex008-operadores-aritm.py

--- Exercicios/ex008-operadores-aritm.py
+++ Exercicios/ex008-operadores-aritm.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite quantos metros deseja converter: '))
 cm = n1*100
 mm = n1*1000
 
-print('O valor em metros é: {}.\nO valor em centímetros é: {}.\nO valor em milímetros é: {}.'.format(n1, cm, mm))
+print('O valor em metros é: {}m.\nO valor em centímetros é: {}cm.\nO valor em milímetros é: {}mm.'.format(n1, cm, mm))
