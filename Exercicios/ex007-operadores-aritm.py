7e192372512d903bd6f28ef43775d4dd0d60f33c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 12:41:54 -0300

    media

---
+++ Exercicios/ex007-operadores-aritm.py
@@ -0,0 +1,6 @@
+n1 = int(input('Digite a primeira nota: '))
+n2 = int(input('Digite a segunda nota: '))
+med = (n1+n2)/2
+
+print('Chegou a hora de saber a sua média final!')
+print('A sua média foi: {}!'.format(med))
