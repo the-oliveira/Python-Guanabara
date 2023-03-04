ea45f7fba7ef9356af46c373b49921f4dc135156
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 14:58:50 -0300

    Update ex010-ope-aritm.py

--- Exercicios/ex010-ope-aritm.py
+++ Exercicios/ex010-ope-aritm.py
@@ -1,8 +1,8 @@
-n1 = float(input('Digite a quantidade de dinheiro: '))
-n2 = float(input('Digite a cotação do dólar atual: '))
+n1 = float(input('Digite a quantidade de dinheiro: R$'))
+n2 = float(input('Digite a cotação do dólar atual: U$'))
 cv = n1/n2
 
-print('Baseado na cotação atual, você conseguirá comprar: {:.2f} dólares.'.format(cv))
+print('Baseado na cotação atual, você conseguirá comprar: U${:.2f}.'.format(cv))
 
 
 
