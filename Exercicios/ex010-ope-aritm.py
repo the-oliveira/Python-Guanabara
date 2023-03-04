146a97073f4111e60a56e99cabb1163d4b393e42
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 13:04:49 -0300

    conversor para dolar

---
+++ Exercicios/ex010-ope-aritm.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite a quantidade de dinheiro: '))
+n2 = float(input('Digite a cotação do dólar atual: '))
+cv = n1/n2
+
+print('Baseado na cotação atual, você conseguirá comprar: {:.2f} dólares.'.format(cv))
+
+
+
