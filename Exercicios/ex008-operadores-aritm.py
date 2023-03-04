3abe47cdedfdf01c2e169a89b1e0ea7fcf04d139
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 12:46:19 -0300

    conversor de metros, cm e mm

---
+++ Exercicios/ex008-operadores-aritm.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite quantos metros deseja converter: '))
+cm = n1*100
+mm = n1*1000
+
+print('O valor em metros é: {}.\nO valor em centímetros é: {}.\nO valor em milímetros é: {}.'.format(n1, cm, mm))
