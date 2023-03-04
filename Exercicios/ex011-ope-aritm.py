6d54ad8546d5860aa13aacba275a452bbd98e24b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 14:02:32 -0300

    calculo de área + quantidade de tinta

---
+++ Exercicios/ex011-ope-aritm.py
@@ -0,0 +1,6 @@
+l = int(input('Digite a largura da parede: '))
+a = int(input('Digite a altura da parede: '))
+area = l*a
+
+print('A área da parede é de: {}m²'.format(area))
+print('A quantidade de tinta para pinta-la é de: {}l.'.format(area/2))
