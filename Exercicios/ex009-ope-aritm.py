a4cf306db6290029051029a3b39108575e045bc8
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 12:52:52 -0300

    tabuada

---
+++ Exercicios/ex009-ope-aritm.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um valor para saber sua tabuada: '))
+
+print('Aqui vai a tabuada do {}: '.format(n1))
+print('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(n1*1, n1*2, n1*3, n1*4, n1*5, n1*6, n1*7, n1*8, n1*9, n1*10))
