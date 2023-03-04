bb0cd3615ec11df85d65abe0a7ad7723d330c819
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-04 14:34:27 -0300

    Update ex008-operadores-aritm.py

--- Exercicios/ex008-operadores-aritm.py
+++ Exercicios/ex008-operadores-aritm.py
@@ -1,5 +1,9 @@
 n1 = float(input('Digite quantos metros deseja converter: '))
+km = n1/1000
+hm = n1/100
+dam = n1/10
+dm = n1*10
 cm = n1*100
 mm = n1*1000
 
-print('O valor em metros é: {}m.\nO valor em centímetros é: {}cm.\nO valor em milímetros é: {}mm.'.format(n1, cm, mm))
+print('valor em quilômetros: {}km. \nValor em hectômetros: {}hm.\nValor em decâmetro: {}dam\nValor em metros: {:.0f}m\nValor em decímetros: {:.0f}dm\nValor em centímetros: {:.0f}cm.\nValor em milímetros: {:.0f}mm.'.format(km, hm, dam, n1,dm, cm, mm))
