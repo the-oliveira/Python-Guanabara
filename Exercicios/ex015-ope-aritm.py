45d319ef753c0d909aa2ee7c988b16a0521dc486
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-05 19:27:30 -0300

    Update ex015-ope-aritm.py

--- Exercicios/ex015-ope-aritm.py
+++ Exercicios/ex015-ope-aritm.py
@@ -1,6 +1,6 @@
 dias = int(input('Quantos dias irá alugar o carro: '))
 km = float(input('Quantos km rodados: '))
-valordia = dias * 60
-valorkm = km * 0.15
+valortotal = (dias * 60) + (km * 0.15)
+
 
 print('O valor total a ser pago é de: R${:.2f}'.format(valordia+valorkm))
