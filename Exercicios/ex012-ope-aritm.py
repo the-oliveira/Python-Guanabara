ad557dd674c77ecd91642bc8bc9d2d807cdafe81
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-05 16:37:27 -0300

    Update ex012-ope-aritm.py

--- Exercicios/ex012-ope-aritm.py
+++ Exercicios/ex012-ope-aritm.py
@@ -1,7 +1,8 @@
-n1 = float(input('Digite o valor do produto em reais: '))
-dc = n1*(5/100)
-vf = n1-dc
+n1 = float(input('Digite o valor do produto: R$'))
+dc = n1-(n1 * 5 / 100)
 
-print('O valor bruto do produto é de R${:.2f}.\nO valor com desconto de 5% a vista é de: R${:.2f}'.format(n1, vf))
+print('O valor bruto do produto é de R${:.2f}.\nO valor com desconto de 5% a vista é de: R${:.2f}'.format(n1, dc))
+
+
 
 
