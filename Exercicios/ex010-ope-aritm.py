cddc7ea5d623306bed1ece8b6872aae28e529a83
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-05 16:25:24 -0300

    Update ex010-ope-aritm.py

--- Exercicios/ex010-ope-aritm.py
+++ Exercicios/ex010-ope-aritm.py
@@ -1,8 +1,16 @@
-n1 = float(input('Digite a quantidade de dinheiro: R$'))
-n2 = float(input('Digite a cotação do dólar atual: U$'))
-cv = n1/n2
+n1 = float(input('Digite a quantidade de dinheiro que deseja converter: R$'))
 
-print('Baseado na cotação atual, você conseguirá comprar: U${:.2f}.'.format(cv))
+dol = n1/5.20
+euro = n1/5.52
+iene = n1/0.0382489625
 
+
+print('='*62)
+print('Agora vamos para a conversão, mas lembre-se, quem converte não se diverte!')
+print('='*62)
+print('Baseado na cotação atual, você conseguirá comprar: U${:.2f}\nSe preferir comprar euro: €{:.2f}\nOu até mesmo se pensa em ir para o japão: ¥{:.2f}.'.format(dol, euro, iene))
+print('='*62)
+print('Boa viagem!!')
+print('='*62)
 
 
