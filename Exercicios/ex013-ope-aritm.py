d6a119bec5d0310c792d7df177babbc219d338d8
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-05 16:41:00 -0300

    Update ex013-ope-aritm.py

--- Exercicios/ex013-ope-aritm.py
+++ Exercicios/ex013-ope-aritm.py
@@ -1,5 +1,5 @@
 salario = float(input('Digite o salário do funcionário: '))
-aumento = salario+(salario*15/100)
+aumento = salario + (salario * 15 / 100)
 
 print('O salário atual do funcionário é de R${:.2f}\nO novo salário com o aumento de 15% será de: R${:.2f}'.format(salario, aumento))
 
