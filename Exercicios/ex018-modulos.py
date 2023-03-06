7915d5df6542506b4d269453b05286c919fef3cd
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 15:32:48 -0300

    Create ex018-modulos.py

---
+++ Exercicios/ex018-modulos.py
@@ -0,0 +1,9 @@
+import math
+
+angulo = int(input('Digite um ângulo: '))
+seno = math.sin(angulo)
+cosseno = math.cos(angulo)
+tangente = math.tan(angulo)
+
+print('O ângulo {}º tem: \nSeno = {}\nCosseno = {}\nTangente = {}.'.format(angulo, seno, cosseno, tangente))
+
