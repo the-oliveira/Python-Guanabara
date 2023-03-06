6d3ddf74d7e24bf5d44df8245f00cc73f0635bdd
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 16:20:46 -0300

    Update ex019-modulos.py

--- Exercicios/ex019-modulos.py
+++ Exercicios/ex019-modulos.py
@@ -3,7 +3,7 @@ a1 = input('Primeiro aluno: ')
 a2 = input('Segundo aluno: ')
 a3 = input('Terceiro aluno: ')
 a4 = input('Quarto aluno: ')
-lista = a1, a2, a3, a4
+lista = [a1, a2, a3, a4]
 sorteio = choice(lista)
 
 print('O aluno escolhido foi: {}'.format(sorteio))
