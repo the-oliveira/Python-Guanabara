e877fa7ceb907fb605a30faf2d561bd5a3f83eed
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 15:51:39 -0300

    .

--- Exercicios/ex016-modulos.py
+++ Exercicios/ex016-modulos.py
@@ -1,6 +1,7 @@
+import math
 from math import floor
 num = float(input('Digite um número real: '))
-int = floor(num)
+int = math.trunc(num)
 
 print('A parte inteira de {} é {:.0f}'.format(num, int))
 
---
+++ Exercicios/ex019-modulos.py
@@ -0,0 +1,8 @@
+import random
+
+aluno1 = str('Jorge Wilsterman')
+aluno2 = str('Anna Carolina')
+aluno3 = str('Adalberto Oliveira')
+aluno4 = str('Eduardo de Oliveira')
+
+print(random)
