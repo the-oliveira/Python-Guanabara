9756564f774c4a682d9729184ee160beaad6b309
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 16:19:47 -0300

    Update ex019-modulos.py

--- Exercicios/ex019-modulos.py
+++ Exercicios/ex019-modulos.py
@@ -1,8 +1,9 @@
-import random
+from random import choice
+a1 = input('Primeiro aluno: ')
+a2 = input('Segundo aluno: ')
+a3 = input('Terceiro aluno: ')
+a4 = input('Quarto aluno: ')
+lista = a1, a2, a3, a4
+sorteio = choice(lista)
 
-aluno1 = str('Jorge Wilsterman')
-aluno2 = str('Anna Carolina')
-aluno3 = str('Adalberto Oliveira')
-aluno4 = str('Eduardo de Oliveira')
-
-print(random)
+print('O aluno escolhido foi: {}'.format(sorteio))
