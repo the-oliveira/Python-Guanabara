fb08777f936b567699c4a2222aeeb1fa1260175b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 16:30:52 -0300

    Update ex020-modulos.py

--- Exercicios/ex020-modulos.py
+++ Exercicios/ex020-modulos.py
@@ -5,6 +5,7 @@ n2 = input('Nome do aluno: ')
 n3 = input('Nome do aluno: ')
 n4 = input('Nome do aluno: ')
 lista = [n1, n2, n3, n4]
-escolha = random.(lista)
+random.shuffle(lista)
 
-print('A ordem de apresentação será: '.format(escolha))
+print('A ordem de apresentação será: {} '.format(lista))
+
