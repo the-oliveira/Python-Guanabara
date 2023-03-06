b4301ff843df06d63b2082f44f8f8852eebc62d8
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 14:42:43 -0300

    Update praticando-módulos.py

--- quarta-aula-utilizando-módulos/praticando-módulos.py
+++ quarta-aula-utilizando-módulos/praticando-módulos.py
@@ -2,4 +2,5 @@ import math
 num = int(input('Digite um número: '))
 raiz = math.sqrt(num)
 
-print('A raiz de {} é igual a: {}'.format(num, raiz))
+print('A raiz de {} é igual a: {}'.format(num, math.ceil(raiz)))
+
