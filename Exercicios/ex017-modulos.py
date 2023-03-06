c0a0ac5d95c22731844c59d73be9f4cb1c74ab7b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-06 16:02:43 -0300

    Update ex017-modulos.py

--- Exercicios/ex017-modulos.py
+++ Exercicios/ex017-modulos.py
@@ -2,6 +2,6 @@ import math
 
 catetoposto = float(input('Digite o valor do cateto oposto: '))
 catetoadj = float(input('Digite o valor do cateto adjacente: '))
-hipotenusa = math.sqrt((catetoadj**2) + (catetoposto**2))
+hipotenusa = math.hypot(catetoposto, catetoadj)
 
 print('O triângulo retângulo possui o cateto oposto = {}, o cateto adjacente = {} e a sua hipotenusa = {:.2f}'.format(catetoposto, catetoadj, hipotenusa))
