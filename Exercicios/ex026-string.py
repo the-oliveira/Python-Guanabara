87ab498afceba293a0791759a4b8659bde662397
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 18:29:12 -0300

    Update ex026-string.py

--- Exercicios/ex026-string.py
+++ Exercicios/ex026-string.py
@@ -1,4 +1,4 @@
 frase = input('Digite uma frase: ')
 print('Quantas vezes a letra A foi utilizada na sua frase: {}.'.format(frase.count('A')))
 print('A primeira posição da letra A é: {}'.format(frase.find('A', 0)))
-print('A ultima posição da letra A é: {}'.format(frase.find('A')))
+print('A ultima posição da letra A é: {}'.format(frase.find('A', -1)))
