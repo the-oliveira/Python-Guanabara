34143f5a5adc97182eef1c9172388ed79aaead0b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 19:10:25 -0300

    Update ex027-string.py

--- Exercicios/ex027-string.py
+++ Exercicios/ex027-string.py
@@ -1,4 +1,4 @@
-nome = input('Digite seu nome completo: ')
+nome = input('Digite seu nome completo: ').strip()
 pnome = nome.split()
 print('Seu primeiro nome é {}.'.format(pnome[0]))
 print('Seu Ultimo nome é {}'.format(pnome[-1]))
