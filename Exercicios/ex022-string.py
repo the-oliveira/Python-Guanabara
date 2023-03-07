ceb1b5fbce98f44713e797917b62294c0328543f
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 18:41:20 -0300

    Update ex022-string.py

--- Exercicios/ex022-string.py
+++ Exercicios/ex022-string.py
@@ -1,11 +1,11 @@
 nome = input('Digite o seu nome completo: ').strip()
 
-print(nome.upper())
-print(nome.lower())
+print('Seu nome todo em maiúsculo: {}.'.format(nome.upper()))
+print('Seu nome em minúsculo {}.'.format(nome.lower()))
 espaco = len(nome) - (nome.count(' '))
-print(espaco)
+print('Seu nome possuí ao todo {} letras.'.format(espaco))
 nome = nome.split()
-print(len(nome[0]))
+print('Seu primeiro nome é {} e ele possuí {} letras.'.format(nome[0], len(nome[0])))
 
 
 
