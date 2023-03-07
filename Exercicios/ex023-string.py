db583198bfc337a8bdfbf00a97898a55bd4e3bfc
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 18:05:37 -0300

    Create ex023-string.py

---
+++ Exercicios/ex023-string.py
@@ -0,0 +1,7 @@
+numero = input('Digite um número de 0 a 9999: ')
+
+print('Milhar = {}'.format(numero[0]))
+print('Centena = {}'.format(numero[1]))
+print('Dezena = {}'.format(numero[2]))
+print('Unidade = {}'.format(numero[3]))
+
