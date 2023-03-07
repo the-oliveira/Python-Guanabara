fb066eaea090684f8c8c8d1d87f4553322dce7f7
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 19:03:57 -0300

    Update ex026-string.py

--- Exercicios/ex026-string.py
+++ Exercicios/ex026-string.py
@@ -1,4 +1,7 @@
-frase = input('Digite uma frase: ')
-print('Quantas vezes a letra A foi utilizada na sua frase: {}.'.format(frase.count('A')))
-print('A primeira posição da letra A é: {}'.format(frase.find('A', 0)))
-print('A ultima posição da letra A é: {}'.format(frase.find('A', -1)))
+from unidecode import unidecode
+com_acento = input('Digite uma frase: ').strip().lower()
+frase = unidecode(com_acento)
+
+print('Quantas vezes a letra A foi utilizada na sua frase: {}.'.format(frase.count('a')))
+print('A primeira posição da letra A é: {}'.format(frase.find('a', 0)))
+print('A ultima posição da letra A é: {}'.format(frase.find('a', -1)))
