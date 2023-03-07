8721512f581e73d0d63ddd98ddcae143511e119a
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-07 18:33:21 -0300

    Update ex022-string.py

--- Exercicios/ex022-string.py
+++ Exercicios/ex022-string.py
@@ -2,6 +2,8 @@ nome = input('Digite o seu nome completo: ')
 
 print(nome.upper())
 print(nome.lower())
+espaco = len(nome) - (nome.count(' '))
+print(espaco)
 nome = (nome.split())
 print(len(nome[0]))
 
