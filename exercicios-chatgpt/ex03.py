5faed764d0eebd6ca7605f075a97859b62390a83
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-08 18:03:08 -0300

    ex03
    Crie um programa que receba duas notas de um aluno e calcule a média.

---
+++ exercicios-chatgpt/ex03.py
@@ -0,0 +1,9 @@
+bimestre1 = float(input('Primeiro bimestre: '))
+bimestre2 = float(input('Segundo bimestre: '))
+bimestre3 = float(input('Terceiro bimestre: '))
+media = (bimestre1+bimestre2+bimestre3)/3
+
+print('='*60)
+print('Olá! Vamos ver agora a sua média no semestre: ')
+print('Sua média foi: {:.2f}'.format(media))
+print('='*60)
