407ca28dc4c56a7ce9927e5fad78e216a574a515
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-08 18:24:15 -0300

    ex6
    Crie um programa que receba um número inteiro de 3 dígitos e exiba a soma de seus dígitos.

---
+++ exercicios-chatgpt/ex06.py
@@ -0,0 +1,9 @@
+numero1 = str(input('Digite um número com 3 digitos: '))
+
+digito1 = int(numero1[0])
+digito2 = int(numero1[1])
+digito3 = int(numero1[2])
+
+soma = digito1+digito2+digito3
+
+print('O número digitado foi {}, a soma dos 3 números é igual a {}.'.format(numero1, soma))
