ce199fb04865eb94d9da64caf51cad50f4cea6b0
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-09 15:39:18 -0300

    ano bissexto

---
+++ Exercicios/ex032-condições.py
@@ -0,0 +1,6 @@
+ano = int(input('Digite um ano: '))
+if ano % 4 == 0 or ano % 400 == 0:
+    print('{} é um ano bissexto!'.format(ano))
+else:
+    print('{} não é bissexto!'.format(ano))
+print('---Fim---')
