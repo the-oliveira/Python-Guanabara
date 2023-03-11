11525385ca90ce3aa78e0602c9db7a392e92a7cf
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-11 12:22:59 -0300

    Update ex034-condições.py

--- Exercicios/ex034-condições.py
+++ Exercicios/ex034-condições.py
@@ -1,6 +1,6 @@
 salario = float(input('Digite o seu salário: R$'))
 
-if salario > 1250:
+if salario >= 1250:
     aumento = (salario*10/100) + salario
     print('Seu salário antigo era R${:.2f}\nSeu novo salário é de R${:.2f}, um aumento de 10%!!\nMeus parabéns!!'.format(salario, aumento))
 else:
