f1b2f565e4a30bcf084a42c4273642c6a9692284
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-11 12:42:15 -0300

    Update ex028-condições.py

--- Exercicios/ex028-condições.py
+++ Exercicios/ex028-condições.py
@@ -1,9 +1,13 @@
 from random import randint
+from time import sleep
+
 sort = randint(0, 5)
 print('='*60)
 print('Irei pensar em um número de 0 a 5, tente advinhar!')
 print('='*60)
 n1 = int(input('Em que número eu pensei? '))
+print('PROCESSANDO...')
+sleep(3)
 if n1 == sort:
     print('O computador escolheu o número {}\nVocê foi sábio e ganhou, PARABÉNS!'.format(sort))
 else:
