5aeea540f9480b7560f6c239b329693b24325df8
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-13 13:33:27 -0300

    ordem de valores

---
+++ Exercicios/ex33- condições.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+
+print('O maior valor digitado foi: {}')
+print('O menor valor digitado foi: {}')
