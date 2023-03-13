19cacfa4f9a226f6d824460edc5c5672ed7d3a8f
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-13 13:31:09 -0300

    Update ex032-condições.py

--- Exercicios/ex032-condições.py
+++ Exercicios/ex032-condições.py
@@ -1,4 +1,7 @@
+from datetime import date
 ano = int(input('Digite um ano: '))
+if ano == 0:
+    ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('{} é um ano bissexto!'.format(ano))
 else:
