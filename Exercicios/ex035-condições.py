b4e318bd38f90e3b9d4322ebad19aedaf8ca616c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-13 13:55:45 -0300

    Update ex035-condições.py

--- Exercicios/ex035-condições.py
+++ Exercicios/ex035-condições.py
@@ -1,13 +1,13 @@
-print('-=-'*20)
+print('=-='*20)
 print('Formando Triângulos!')
-print('-=-'*20)
+print('=-='*20)
 
 n1 = float(input('Valor da primeira reta: '))
 n2 = float(input('Valor da segunda reta: '))
 n3 = float(input('Valor da terceira reta: '))
 
 
-if n1 < n2+n3 and n2 < n1+n3 and n3 < n1+n2:
+if n1 < n2 + n3 and n2 < n1 + n3 and n3 < n1 + n2:
     print('O segmento pode formar um triângulo!!')
 else:
     print('O segmento não pode formar um triângulo!!')
