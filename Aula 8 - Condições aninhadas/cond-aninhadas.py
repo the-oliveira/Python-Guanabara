532a569ec6aa4ee9113c0bca478ba1ec2b5bee3c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-14 18:08:26 -0300

    Update cond-aninhadas.py

--- Aula 8 - Condições aninhadas/cond-aninhadas.py
+++ Aula 8 - Condições aninhadas/cond-aninhadas.py
@@ -4,10 +4,8 @@ if nome == 'Eduardo':
     print('Você tem um belo nome!')
 elif nome == 'Anna':
     print('Você tem um nome lindo!')
-elif nome == 'Kaue':
+elif nome == 'Kaue' or nome =='Guilherme' or nome =='Gustavo':
     print('Você é calvo')
-elif nome == 'Gustavo':
-    print('Você é careca')
 else:
     print('Seu nome é comum!')
 print('Tenha um excelente dia, \033[1:61:40m{}\033[m!'.format(nome))
