a7507a042f5d6983fb59826c71981cf90375c7f1
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-14 11:33:34 -0300

    Update cores.py

---
+++ sétima-aula-cores/cores.py
@@ -0,0 +1,6 @@
+#EXEMPLO DO COMANDO: \033[XX:XX:XXm
+
+#primeiro valor após o '[' é referente ao estilo, possuindo principalmente o 0(nenhum) 1(bold) 4(underline) e 7(negativo)
+#Segundo valor, após o estilo, é o da cor do texto: 30(preto) 31(vermelho) 32(verde) 33(amarelo) 34(azul) 35(magenta) 36(ciano) 37(cinza) e 97(branco)
+#Terceiro valor, cor do background: 40(preto) 41(vermelho) 42(verde) 43(amarelo) 44(azul) 45(magenta) 46(ciano) 47(cinza) 107(branco)
+
