41844521badfe3e8781c640e5c142bfea6d4bd4d
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-14 21:58:16 -0300

    Update ex042-condições-aninhadas.py

--- Exercicios/ex042-condições-aninhadas.py
+++ Exercicios/ex042-condições-aninhadas.py
@@ -9,7 +9,7 @@ if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
     print('\033[1:32mOs valores de {:.2f}, {:.2f} e {:.2f} podem formar um triângulo!\033[m'.format(reta1, reta2, reta3))
 else:
     print('\033[1:31mOs valores {:.2f}, {:.2f} e {:.2f} não podem formar um triângulo!\033[m'.format(reta1, reta2, reta3))
-
+    exit()
 if reta1 == reta2 and reta1 == reta3 and reta2 == reta3:
     print('O triângulo formado é um \033[4:34mEquilátero\033[m.')
 elif reta1 == reta2 and reta2 != reta3 and reta1 != reta3 or reta2 == reta3 and reta1 != reta2 and reta1 != reta3 or reta3 == reta1 and reta3 != reta2 and reta1 != reta2:
