b6b5d0b60ba05f537ad5027fc0afb7f064b32c81
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 16:15:20 -0300

    Update ex038-condições-aninhadas.py

--- Exercicios/ex038-condições-aninhadas.py
+++ Exercicios/ex038-condições-aninhadas.py
@@ -1,9 +1,9 @@
-n1 = float(input('Primeiro valor: '))
-n2 = float(input('Segundo valor: '))
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
 
 if n1 > n2:
-    print('O primeiro valor, que é {:.2f} é o maior valor.'.format(n1))
+    print('O primeiro valor, que é {:.0f} é o maior valor.'.format(n1))
 elif n2 > n1:
-    print('O segundo valor, que é {:.2f} é o maior valor'.format(n2))
+    print('O segundo valor, que é {:.0f} é o maior valor'.format(n2))
 else:
     print('Os valores são iguais!')
