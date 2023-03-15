6d4d668d9aba91af0e163dd8efa0471631731d81
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 16:20:23 -0300

    Update ex039-condições-aninhadas.py

--- Exercicios/ex039-condições-aninhadas.py
+++ Exercicios/ex039-condições-aninhadas.py
@@ -4,9 +4,9 @@ data = int(input('Você nasceu em que ano? '))
 idade = datetime.date.today().year - data
 
 if idade < 18:
-    print('Olá {}, \033[1:31mainda não está na sua hora de se alistar\033[m, porém daqui {} anos você poderá se alistar!'.format(nome, 18-idade))
+    print('Olá {}, \033[1:31mainda não está na sua hora de se alistar\033[m, porém daqui {} anos você poderá se alistar!\nVocê irá se alistar no ano de {}'.format(nome, 18-idade, data+18))
 elif idade == 18:
     print('Olá {}, \033[1:32mestá na hora de se alistar!\033[m\nVocê completa 18 anos este ano então é hora de se alistar, boa sorte!'.format(nome))
 elif idade > 18:
-    print('Olá {}, você já se alistou? pois ja se passaram {} ano(s) do seu ano de alistamento!'.format(nome, idade-18))
+    print('Olá {}, você já se alistou? pois ja se passaram {} ano(s) do seu ano de alistamento!\nSeu alistamento foi no ano de {}.'.format(nome, idade-18, data+18))
 print('Tenha um excelente dia!')
