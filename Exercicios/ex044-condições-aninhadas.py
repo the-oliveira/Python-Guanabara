4d29fc07f8bbf3002b4c295b777c9a54771b5109
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 17:14:34 -0300

    Update ex044-condições-aninhadas.py

--- Exercicios/ex044-condições-aninhadas.py
+++ Exercicios/ex044-condições-aninhadas.py
@@ -6,6 +6,9 @@ print('''O produto possui o valor de R${:.2f}, você pode escolher entre:
 [3] Em 2x no cartão com o preço normal (digite 3).
 [4] Em 3x ou mais no cartão com 20% de juros (digite 4).'''.format(produto))
 pagamento = int(input('Escolha a forma de pagamento (1-4): '))
+if pagamento < 1 or pagamento > 4:
+    print('\033[1:31mValor incorreto, tente novamente.\033[m')
+    exit()
 
 if pagamento == 1:
     print('Valor do produto: R${:.2f} será pago a vista e com 10% de deconto no dinheiro ou cheque, portanto o novo valor do produto é de R${:.2f}'.format(produto, produto - (produto * 10/100)))
@@ -16,7 +19,7 @@ elif pagamento == 3:
 elif pagamento == 4:
     parcelas = int(input('Digite o número de parcelas em até 12x: '))
     if parcelas > 12:
-        print('Número de parcelas inválida, tente novamente.')
+        print('\033[1:31mNúmero de parcelas excede o maximo de 12x, tente novamente.\033[m')
         exit()
     else:
         juros = produto + (produto * 20/100)
