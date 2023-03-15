1c5ac962bd93ffb79dfff7d18c8f040a5d4a7238
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 16:39:31 -0300

    Update ex041-condições-aninhadas.py

--- Exercicios/ex041-condições-aninhadas.py
+++ Exercicios/ex041-condições-aninhadas.py
@@ -5,9 +5,9 @@ idade = datetime.date.today().year - nascimento
 
 if idade <= 9:
     print('O atleta {} pertence a categoria MIRIM.'.format(nome))
-elif idade == 10 or idade == 11 or idade == 12 or idade == 13 or idade == 14:
+elif idade <= 14:
     print('O atleta {} pertence a categoria INFANTIL.'.format(nome))
-elif idade == 15 or idade == 16 or idade == 17 or idade == 18 or idade == 19:
+elif idade <= 19:
     print('O atleta {} pertence a categoria JUNIOR.'.format(nome))
 elif idade == 20:
     print('O atleta {} pertence a categoria SÊNIOR.'.format(nome))
