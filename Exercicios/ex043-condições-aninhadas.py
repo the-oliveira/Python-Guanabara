f85af9c54afddde18d0189a18a4830426c3ef17d
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 16:55:05 -0300

    Update ex043-condições-aninhadas.py

--- Exercicios/ex043-condições-aninhadas.py
+++ Exercicios/ex043-condições-aninhadas.py
@@ -4,12 +4,12 @@ imc = peso/(altura * altura)
 
 if imc < 18.5:
     print('Seu IMC é:{:.1f} portanto você está abaixo do peso '.format(imc))
-elif imc < 25:
+elif imc >= 18.5 or imc < 25:
     print('Seu IMC é:{:.1f} portanto você está no peso ideal '.format(imc))
-elif imc < 30:
+elif imc >= 25 or imc < 30:
     print('Seu IMC é:{:.1f} portanto você está sobrepeso '.format(imc))
-elif imc < 40:
+elif imc >= 30 or imc < 40:
     print('Seu IMC é:{:.1f} portanto você está com obesidade '.format(imc))
-elif imc > 40:
+elif imc >= 40:
     print('Seu IMC é:{:.1f} portanto você está com obesidade mórbida '.format(imc))
     
