e25d403141041691c74a44defa31a3678da2340c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-15 17:44:25 -0300

    Update ex045-condições-aninhadas.py

--- Exercicios/ex045-condições-aninhadas.py
+++ Exercicios/ex045-condições-aninhadas.py
@@ -1,16 +1,8 @@
 from random import randint
-jogador = str(input('Pedra, papel ou tesoura? '))
-jogador.lower()
+print('''Faça sua escolha: 
+[ 1 ] PEDRA
+[ 2 ] PAPEL
+[ 3 ] TESOURA''')
+escolha = int(input('Faça sua escolha: '))
 computador = randint(1, 3)
-
-if computador == 1:
-    jokenpo = str('pedra')
-elif computador == 2:
-    jokenpo = str('papel')
-elif computador == 3:
-    jokenpo = str('tesoura')
-
-if computador == 'pedra' and jogador == 'pedra' or computador == 'papel' and jogador == 'papel' or computador == 'tesoura' and computador == 'tesoura':
-    print('Empatou!')
-
 
