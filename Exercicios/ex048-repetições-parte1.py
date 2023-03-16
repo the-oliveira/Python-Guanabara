34b0d3da31d3adc4ae1534d99e05539ea4dd9c2c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 17:38:05 -0300

    Update ex048-repetições-parte1.py

--- Exercicios/ex048-repetições-parte1.py
+++ Exercicios/ex048-repetições-parte1.py
@@ -1,6 +1,6 @@
 soma = 0
 for c in range(0, 500):
-    if c % 3 == 0:
+    if c % 2 == 1 and c % 3 == 0:
         soma = soma + c
 
 print('A soma de todos os números impares multiplos de 3 de 0 a 500 é : {}'.format(soma))
