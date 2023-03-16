ea6a26008c4a3971f42873fb97c88153aa6cdf07
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 17:59:37 -0300

    Create ex051-repetições-parte1.py

---
+++ Exercicios/ex051-repetições-parte1.py
@@ -0,0 +1,8 @@
+print('Representação de uma Progressão Aritmética.')
+pt = int(input('Digite o valor do primeiro termo: '))
+razao = int(input('Digite a razão: '))
+pa = 0
+for c in range(pt, pt + 10, razao):
+    print(c)
+
+
