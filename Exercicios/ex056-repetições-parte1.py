83ba357430b6985b409b60cf584bbe371dfbe49b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 18:42:09 -0300

    Update ex056-repetições-parte1.py

--- Exercicios/ex056-repetições-parte1.py
+++ Exercicios/ex056-repetições-parte1.py
@@ -1,12 +1,13 @@
 med = 0
 vinte = 0
 for c in range(0,4):
-    nome = str(input('Qual o seu nome? '))
-    idade = int(input('Qual a sua idade? '))
+    nome = str(input('NOME: '))
+    idade = int(input('IDADE: '))
     med += idade
-    sexo = str(input('Qual o seu sexo? ')).lower()
+    sexo = str(input('SEXO: ')).lower()
     if sexo == 'feminino' and idade > 20:
-        vinte += 1
+        vinte = vinte + 1
+    print('='*10)
 
 print('A média de idade do grupo é de: {:.1f}'.format(med/4))
 print('O número de mulheres com menos de 20 anos é de: {}'.format(vinte))
