e3728ec73cdd8470fd2da98990a836bda09fe3c9
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 19:01:05 -0300

    Update ex049-repetições-parte 1.py

--- Exercicios/ex049-repetições-parte 1.py
+++ Exercicios/ex049-repetições-parte 1.py
@@ -2,5 +2,5 @@ tabuada = int(input('Qual tabuada deseja saber? '))
 print('*'*30)
 for c in range(1,10+1):
     m = tabuada * c
-    print('{} x {} = {}'.format(tabuada, c, m))
+    print('{} x {:2} = {:2}'.format(tabuada, c, m))
 print('*'*30)
