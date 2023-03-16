d2c5bf231c611f7598f94020f196021de6610162
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 18:50:43 -0300

    Update ex047-repetições-parte1.py

--- Exercicios/ex047-repetições-parte1.py
+++ Exercicios/ex047-repetições-parte1.py
@@ -1,8 +1,5 @@
 n = 0
-for c in range(0, 50):
-    n = c + 1
-    if n % 2 == 0:
-        print('\033[1:32m{} é par\033[m'.format(n))
-    else:
-        print('\033[1:31m{} é impar\033[m'.format(n))
+for c in range(2, 51, 2):
+    if c % 2 == 0:
+        print(c, end=' ')
 print('Fim')
