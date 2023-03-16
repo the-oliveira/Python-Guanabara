2f888418255809f7a545c03e676fffb64eaf3bd1
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 17:18:07 -0300

    Update ex046-repetições-parte1.py

--- Exercicios/ex046-repetições-parte1.py
+++ Exercicios/ex046-repetições-parte1.py
@@ -1,4 +1,6 @@
 from time import sleep
+import emoji
+
 print('='*50)
 print('\033[1:31:40mVamos nos preparar para a virada do ano novo!!\033[m')
 print('='*50)
@@ -6,4 +8,4 @@ sleep(1)
 for c in range(10, 0-1, -1):
     print('\033[1:36m', c, '\033[m')
     sleep(1)
-print('\033[1:31:40mFELIZ ANO NOVO!!\033[m')
+print(emoji.emojize("\033[1:31:40mFELIZ ANO NOVO :red_heart:!!\033[m"))
