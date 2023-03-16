b0daa127cbbdccf9d5fa4e6091b55f218f301002
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 18:22:40 -0300

    Create ex054-repetições-parte1.py

---
+++ Exercicios/ex054-repetições-parte1.py
@@ -0,0 +1,16 @@
+import datetime
+
+maior = 0
+menor = 0
+presente = datetime.date.today().year
+
+for c in range(0,7):
+    ano = int(input('Ano de nascimento: '))
+    if presente - ano >= 18:
+        maior = maior + 1
+    else:
+        menor = menor + 1
+
+print('{} são maiores de idade.'.format(maior))
+print('{} são menores de idade.'.format(menor))
+
