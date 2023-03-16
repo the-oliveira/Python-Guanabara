5111a67c80a47deffb0fc71101fab34a8cbacda4
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 18:55:47 -0300

    Update ex048-repetições-parte1.py

--- Exercicios/ex048-repetições-parte1.py
+++ Exercicios/ex048-repetições-parte1.py
@@ -3,4 +3,4 @@ for c in range(1, 501, 2):
     if c % 2 == 1 and c % 3 == 0:
         soma = soma + c
 
-print('A soma de todos os números impares multiplos de 3 de 0 a 500 é : {}'.format(soma))
+print('A soma de todos os números impares multiplos de 3 de 1 a 500 é : {}'.format(soma))
