802acd6255635a7b9eaee19a95d9e7abd8b8249c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 22:51:10 -0300

    Update ex052-repetições-parte1.py

--- Exercicios/ex052-repetições-parte1.py
+++ Exercicios/ex052-repetições-parte1.py
@@ -3,12 +3,13 @@ div = 0
 for c in range(1, n + 1):
     if n % c == 0:
         print(f'\033[1:31m{c} \033[m', end='')
-        div += 1
+        div = div + 1
     else:
         print(f'\033[1:32m{c} \033[m', end='')
 
 print(f'\nO número {n} foi divisível {div} vezes, portanto:')
-if div > 2:
-    print('Ele \033[1:31mNÃO É PRIMO\033[m')
-else:
+
+if div == 2:
     print('Ele \033[1:32mÉ PRIMO\033[m')
+else:
+    print('Ele \033[1:31mNÃO É PRIMO\033[m')
