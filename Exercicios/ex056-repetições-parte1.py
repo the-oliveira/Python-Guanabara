503c4e047e5aa16e828e5a7a4b9f6718ed5f7e42
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-17 12:28:37 -0300

    Update ex056-repetições-parte1.py

--- Exercicios/ex056-repetições-parte1.py
+++ Exercicios/ex056-repetições-parte1.py
@@ -1,22 +1,25 @@
 med = 0
 vinte = 0
-lista = []
-nomelist = []
+maioridade = 0
+nomevelho = ''
+
 for c in range(1,5):
     nome = str(input('NOME: ')).strip()
     idade = int(input('IDADE: '))
     med += idade
     sexo = str(input('SEXO [M/F]: ')).lower().strip()
 
-    if sexo == 'f' and idade > 20:
+    if sexo == 'f' and idade < 20:
         vinte = vinte + 1
-    elif sexo == 'm':
-        lista += [idade]
-        nomelist += [nome]
+    elif sexo == 'm' and c == 1:
+        maioridade = idade
+        nomevelho = nome
+    if sexo == 'm' and idade > maioridade:
+        maioridade = idade
+        nomevelho = nome
     print('='*10)
 
-lista.sort()
-print(lista)
-print(nomelist)
-print('A média de idade do grupo é de: {:.1f}'.format(med/4))
-print('O número de mulheres com menos de 20 anos é de: {}'.format(vinte))
+med = med/4
+print(f'O nome do homem mais velho é {nomevelho} e a sua idade é {maioridade:.0f}')
+print(f'A média de idade do grupo é de: {med:.1f}')
+print(f'O grupo possui {vinte} mulher(es) com menos de 20 anos.')
