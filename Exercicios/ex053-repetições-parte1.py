30a5b16d7ecb8d3115a119391974519c84abf887
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-16 23:01:35 -0300

    .

--- Exercicios/ex053-repetições-parte1.py
+++ Exercicios/ex053-repetições-parte1.py
@@ -1,2 +1,12 @@
-frase = str(input('Digite uma frase: ')).strip()
-print()
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) - 1, -1,-1):
+    inverso += junto[letra]
+
+if inverso == junto:
+    print(f'A frase {junto} ao contrario fica {inverso}\nESSA FRASE FORMOU UM PALÍNDROMO!')
+else:
+    print(f'A frase {junto} ao contrario fica {inverso}\nNÃO FORMOU UM PALÍNDROMO!')
+
