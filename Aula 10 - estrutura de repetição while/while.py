09e56a5889fdc9a3c6a606762a200af2b2fdd997
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-18 12:18:33 -0300

    Update while.py

--- Aula 10 - estrutura de repetição while/while.py
+++ Aula 10 - estrutura de repetição while/while.py
@@ -1,3 +1,12 @@
 c = 1
+par = 0
+impar = 0
 while c != 0:
     c = float(input('Digite um número: '))
+    if c != 0:
+        if c % 2 == 0:
+            par += 1
+        else:
+            impar +=1
+
+print(f'Foram digitados {par} valores pares e {impar} valores impares.')
