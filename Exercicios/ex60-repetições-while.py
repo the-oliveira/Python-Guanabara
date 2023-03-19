f18cabf2aa7814aa301fc45f31ed6d5a9e89005f
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-19 18:10:44 -0300

    Create ex60-repetições-while.py

---
+++ Exercicios/ex60-repetições-while.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um valor: '))
+fatorial = n1 - 1
+lista = []
+while fatorial != 0:
+    mult = n1 * fatorial
+    n1 -= 1
+    lista = []
+print(f'O fatorial de {n1} é: ')
+print(lista)
