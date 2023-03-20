5a5e7f5ef45f62fe39ff27e11543e092fb9ba2a3
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-20 12:19:18 -0300

    Update ex058-repetição-while.py

--- Exercicios/ex058-repetição-while.py
+++ Exercicios/ex058-repetição-while.py
@@ -3,7 +3,11 @@ tentativas = 1
 rand = randint(0, 10)
 n = int(input('Advinhe o número que o PC pensou de 1 a 10: '))
 while n != rand:
-    n = int(input('Você errou, tente novamente: '))
+    if n > rand:
+        print('Talvez um pouco menor...')
+    if n < rand:
+        print('Talvez um pouco maior...')
+    n = int(input('Tente novamente: '))
     tentativas += 1
 
 print(f'Você finalmente acertou! O número era {rand} e você acertou em {tentativas} tentativas.')
