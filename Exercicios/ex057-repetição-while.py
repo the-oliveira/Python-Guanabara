235da1dabf8fa5d6aa93e7d7f2ecd386ca84de33
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-20 12:13:27 -0300

    Update ex057-repetição-while.py

--- Exercicios/ex057-repetição-while.py
+++ Exercicios/ex057-repetição-while.py
@@ -1,11 +1,11 @@
-g = ''
-while g != 'M' and g != 'F':
-    g = str(input('Qual o seu sexo? [M/F] ')).strip().upper()
-    if g != 'M' and g != 'F':
+genero = ''
+while genero != 'M' and genero != 'F':
+    genero = str(input('Qual o seu sexo? [M/F] ')).strip().upper()[0]
+    if genero != 'M' and genero != 'F':
         print('\033[1:31mComando inválido, tente novamente!\033[m')
 
 
-if g == 'M':
+if genero == 'M':
     print('\033[1:36mSexo masculino registrado com sucesso!\033[m')
 else:
     print('\033[1:35mSexo feminino registrado com sucesso!\033[m')
