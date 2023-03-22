ecbef7232fae870f8abaf4e23fb4a0c6732449cf
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-22 17:23:16 -0300

    Update ex051-repetições-parte1.py

--- Exercicios/ex051-repetições-parte1.py
+++ Exercicios/ex051-repetições-parte1.py
@@ -1,7 +1,7 @@
 print('Representação de uma Progressão Aritmética.')
 ptermo = int(input('Digite o valor do primeiro termo: '))
 razao = int(input('Digite a razão: '))
-decimo = ptermo + (10 - 1) * razao
+decimo = ptermo + (11 - 1) * razao
 for c in range(ptermo, decimo, razao):
     print(f'{c}', end=' → ')
 
