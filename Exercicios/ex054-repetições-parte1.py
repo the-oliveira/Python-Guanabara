52cfdcb2fe13f55aae5191c616c700d0e2b6f179
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-03-22 18:14:45 -0300

    Update ex054-repetições-parte1.py

--- Exercicios/ex054-repetições-parte1.py
+++ Exercicios/ex054-repetições-parte1.py
@@ -1,16 +1,13 @@
 import datetime
-
 maior = 0
 menor = 0
 presente = datetime.date.today().year
-
 for pessoas in range(1,8):
     ano = int(input(f'Ano de nascimento da {pessoas}ª pessoa: '))
     if presente - ano >= 21:
         maior = maior + 1
     else:
         menor = menor + 1
-
 print(f'Ao todo {maior} são maiores de idade.')
 print(f'E também tivemos {menor} menores de idade.')
 
