6157384ee77cf413a390f67e1a82bfab96a6f191
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-04 18:51:48 -0300

    Update ex062-repetições-while.py

--- Exercicios/ex062-repetições-while.py
+++ Exercicios/ex062-repetições-while.py
@@ -1,17 +1,14 @@
 ptermo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão da PA: '))
 termos = 0
-
-while termos < 10:
-    print(f'{ptermo + termos * razao }', end=' - ')
-    termos += 1
-    if termos == 10:
-        ltermo = ptermo
-        continuar = str(input('Deseja ver mais termos? [S/N]: ')).lower().strip()
-        if continuar == 's':
-            ntermos = int(input('Quantos termos deseja ver a mais? '))
-        else:
-            print('Fim do programa!')
-            exit()
-
-print('Final!')
+continuar = 10
+total = 0
+contador = 0
+while continuar != 0:
+    total += continuar
+    while termos < total:
+        print(f'{ptermo + termos * razao }', end=' - ')
+        termos += 1
+    print('Parando...')
+    continuar = int(input('Deseja ver mais quantos termos? '))
+print(f'Fim do programa, você viu {total} termos desta PA.')
