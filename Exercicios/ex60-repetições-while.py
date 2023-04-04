15ac664ff99c57bc57b0ca0dc60d507fb9018ed5
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-04 18:31:24 -0300

    Update ex60-repetições-while.py

--- Exercicios/ex60-repetições-while.py
+++ Exercicios/ex60-repetições-while.py
@@ -1,10 +1,18 @@
+from math import factorial
 n = int(input('Digite um número: '))
 contador = n
 mult = 1
 print(f'Vamos calcular o fatorial de {n}! ')
-while contador > 0:
+'''while contador > 0:
     print(f'{contador}', end='')
     print(' x ' if contador > 1 else ' = ', end='')
     mult *= contador
     contador -= 1
-print(mult)
+print(mult)'''
+#Utilizando o for para realizar o mesmo programa
+
+for c in range(n,0,-1):
+    print(f'{contador}', end='')
+    print(' x ' if contador > 1 else' = ', end='')
+    contador -= 1
+print(factorial(n))
