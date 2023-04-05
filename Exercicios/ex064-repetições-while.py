ed4f94a2db5de12af5a5b3868aefb4176b11a393
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-05 18:40:51 -0300

    Update ex064-repetições-while.py

--- Exercicios/ex064-repetições-while.py
+++ Exercicios/ex064-repetições-while.py
@@ -1,12 +1,12 @@
-numeros = 0
+total = 0
 n1 = 0
 numero = 0
 while numero != 999:
-    numeros += 1
+    total += 1
     numero = int(input('Digite um número (digite 999 se deseja finalizar a soma): '))
     if numero == 999:
         n1 -= 999
-        numeros -= 1
+        total -= 1
     n1 = numero + n1
 
-print(f'Você digitou {numeros} numeros e a soma de todos eles é igual a {n1}')
+print(f'Você digitou {total} numeros e a soma de todos eles é igual a {n1}')
