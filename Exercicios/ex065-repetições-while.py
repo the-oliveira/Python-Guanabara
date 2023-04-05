8131929a9513b8ee0544c599e5bb087ed6a99a42
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-05 18:59:35 -0300

    Update ex065-repetições-while.py

--- Exercicios/ex065-repetições-while.py
+++ Exercicios/ex065-repetições-while.py
@@ -3,20 +3,16 @@ med = 0
 maior = 0
 menor = 0
 termos = 1
-
+lista = []
 while continuar == 's':
     n1 = int(input('Digite um número: '))
+    lista.append(n1)
     med = med + n1
     termos += 1
-    if termos > 0:
-        if n1 > maior:
-            maior = n1
-        if n1 < maior:
-            menor = n1
     continuar = str(input('Deseja continuar? [S/N] ')).lower().strip()
 
 
 media = med/(termos - 1)
 print(f'A média entre todos os termos digitado é de {media}')
-print(f'O maior número digitado foi {maior}')
-print(f'O menor número digitado foi {menor}')
+print(f'O maior número digitado foi {max(lista, key=int)}')
+print(f'O menor número digitado foi {min(lista, key=int)}')
