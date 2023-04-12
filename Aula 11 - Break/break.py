e56896885b8380f25728c5fbf1f89f86b367546c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-12 18:21:00 -0300

    Update break.py

--- Aula 11 - Break/break.py
+++ Aula 11 - Break/break.py
@@ -1,8 +1,7 @@
-x = True
-oi = 0
-while True:
-    print('Olá!')
-    oi += 1
-    if oi == 4:
-        print('Acabou')
+n = s = 0
+while n != 999:
+    n = int(input('Digite um número (999 para finalizar): '))
+    if n == 999:
         break
+    s += n
+print(f'A soma dos números digitados é igual a {s}')
