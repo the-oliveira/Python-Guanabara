0cb7ff100fc39a9d821e0028c6fa164e010efc4f
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-12 19:19:05 -0300

    Create ex067-break.py

---
+++ Exercicios/ex067-break.py
@@ -0,0 +1,12 @@
+cont = 1
+tabuada = int(input('Digite a tabuada que deseja ver: '))
+while cont <= 11:
+    mult = tabuada*cont
+    print(f'{tabuada} x {cont} = {mult}')
+    cont += 1
+    if cont == 11:
+        cont = 1
+        tabuada = int(input('Digite a tabuada que deseja ver: '))
+    if tabuada < 0:
+        break
+print('Fim do programa! Volte sempre!')
