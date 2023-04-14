d3904edd71b0ca698cc9d23a32b2c2b6f3ed3acd
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-14 17:03:05 -0300

    Update ex070-break.py

--- Exercicios/ex070-break.py
+++ Exercicios/ex070-break.py
@@ -4,20 +4,25 @@ cont = 0
 menor = 0
 maisbarato = ''
 while True:
+    print('='*50)
     nomeproduto = str(input('Nome do produto: ')).strip()
     valor = float(input('Valor do produto: R$'))
+    print('=' * 50)
     if valor >= 1000:
         maisde1k += 1
     if cont == 0:
         maisbarato = nomeproduto
         menor = valor
-    if valor <= menor:
+    if cont >= 1 and valor <= menor:
         maisbarato = nomeproduto
         menor = valor
     total += valor
+    cont += 1
     continuar = str(input('Deseja comprar mais algo? [S/N] ')).strip().upper()[0]
     if continuar == 'N':
         break
+print('='*50)
 print(f'O valor total gaasto na compra foi de R${total:.2f}.')
 print(f'O produto mais barato foi o(a) {maisbarato}, custando R${menor:.2f}')
 print(f'{maisde1k} produto(s) custaram mais de R$1000.')
+print('='*50)
