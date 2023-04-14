fcf9d95735603b82cb47d2ab2da84ac59fccc268
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-14 18:12:13 -0300

    Update ex069-break.py

--- Exercicios/ex069-break.py
+++ Exercicios/ex069-break.py
@@ -2,6 +2,9 @@ mais18 = 0
 homens = 0
 mulheres20 = 0
 while True:
+    print('='*20)
+    print('CADASTRO DE CLIENTES.')
+    print('=' * 20)
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
     if sexo != 'M' and sexo != 'F':
@@ -17,6 +20,8 @@ while True:
         continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
     if continuar == 'N':
         break
+print('=' * 80)
 print(f'Dentre os clientes cadastrados {mais18} são maiores de 18 anos, tanto do sexo masculino quanto feminino.')
 print(f'Sendo ao todo {mulheres20} mulher(es) menor(es) de 20 anos.')
 print(f'E também consta nos registros {homens} homen(s) cadastrados.')
+print('=' * 80)
