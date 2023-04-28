9216f13eb4d1cd582a674bd0d9df372e4c3caca1
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-28 16:01:03 -0300

    Update tuplas.py

--- Aula 12 - Tuplas/tuplas.py
+++ Aula 12 - Tuplas/tuplas.py
@@ -4,3 +4,12 @@ lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 for c in lanche:
     print(f'Bora pedir um(a) {c}')
 print('Agora sim')
+
+#se precisar da posição, usar:
+
+# for posição, c in enumerate(lanche):
+#   print(f'Para mostrar o elemento da tupla {c} e a posição {posição}'
+
+#Ou utilizar o len em uma estrutura for:
+# for cont in range (0, len(lanche)):
+#   print(f'valor da tupla {lanche[cont]} na posição {cont}'
