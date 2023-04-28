a67317df6aa29e17b46f7691f89723ec7e9168e0
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-28 16:07:27 -0300

    Update tuplas.py

--- Aula 12 - Tuplas/tuplas.py
+++ Aula 12 - Tuplas/tuplas.py
@@ -3,13 +3,20 @@
 lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
 for c in lanche:
     print(f'Bora pedir um(a) {c}')
+    #é possivel usar o print(sorted) para printar a tupla em ordem.
 print('Agora sim')
 
-#se precisar da posição, usar:
 
+#se precisar da posição, usar:
 # for posição, c in enumerate(lanche):
 #   print(f'Para mostrar o elemento da tupla {c} e a posição {posição}'
 
 #Ou utilizar o len em uma estrutura for:
 # for cont in range (0, len(lanche)):
 #   print(f'valor da tupla {lanche[cont]} na posição {cont}'
+
+#É possível agregar o valor de duas tuplas, criando separadamente e depois criando uma terceira com o valor de a + b ou b + a.
+#Podemos usar o print(variavel.count(valor)) caso precisamos ver quantas vezes certo valor é encontrado dentro da tupla.
+#Também existe o print(variavel.index(valor)), que nos mostra a posição em que o valor se encontra na tupla.
+
+#É possível deletar uma variavel utilizando o:  del(variavel)
