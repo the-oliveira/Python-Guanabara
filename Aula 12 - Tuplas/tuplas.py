e298fadbb662b3d26dfd6aa3c8a4e24dd02d230d
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-28 15:51:07 -0300

    Update tuplas.py

--- Aula 12 - Tuplas/tuplas.py
+++ Aula 12 - Tuplas/tuplas.py
@@ -1,3 +1,4 @@
 #Variaveis compostas = armazenam mais de um valor na variavel
 #As tuplas são imutaveis (não podem ser modificadas durante o funcionamento do programa)
-
+lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim'
+print(lanche)
