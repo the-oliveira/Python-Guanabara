ef1126b51d15cb58cae2cd11dc16d8985f4a7d58
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-29 16:06:48 -0300

    Update ex072-Tuplas.py

--- Exercicios/ex072-Tuplas.py
+++ Exercicios/ex072-Tuplas.py
@@ -1,5 +1,12 @@
 numero = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
-escolha = int(input('Digite um número de 0 a 20: '))
-while escolha not in range(0, 21):
-    escolha = int(input('Tente novamente. Digite um número de 0 a 20: '))
-print(f'O número escolhido se escreve da seguinte forma: {numero[escolha]}')
+while True:
+    escolha = int(input('Digite um número de 0 a 20: '))
+    while escolha not in range(0, 21):
+        escolha = int(input('Tente novamente. Digite um número de 0 a 20: '))
+    print(f'O número escolhido se escreve da seguinte forma: {numero[escolha]}')
+    continuar = str(input('Deseja ver outro número? [S/N]: ')).strip().upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Deseja ver outro número? [S/N]: ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print('Volte sempre!')
