26bbf0da55466b3a02cf049b984e3e686b92ee13
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-29 15:59:27 -0300

    .

--- Exercicios/ex072-Tuplas.py
+++ Exercicios/ex072-Tuplas.py
@@ -1,4 +1,4 @@
-numero = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
+numero = 'Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Catorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte'
 escolha = int(input('Digite um número de 0 a 20: '))
 while escolha not in range(0, 21):
     escolha = int(input('Tente novamente. Digite um número de 0 a 20: '))
