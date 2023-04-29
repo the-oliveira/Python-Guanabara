f3c6ab86793c0851828d5ffb33ce1920a9626d64
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-04-29 15:52:24 -0300

    Create ex076-Tuplas.py

---
+++ Exercicios/ex076-Tuplas.py
@@ -0,0 +1,11 @@
+produtos = 'Mangá', 9.99, 'Livro', 23.50, 'Teclado', 234.99, 'Mouse', 99.99, 'Mousepad', 34.50
+print('-'*50)
+print('LISTAGEM DE PRODUTOS')
+print('-'*50)
+print(f'{produtos[0]}....................R${produtos[1]:.2f}')
+print(f'{produtos[2]}....................R${produtos[3]:.2f}')
+print(f'{produtos[4]}....................R${produtos[5]:.2f}')
+print(f'{produtos[6]}....................R${produtos[7]:.2f}')
+print(f'{produtos[8]}....................R${produtos[9]:.2f}')
+print('-'*50)
+
