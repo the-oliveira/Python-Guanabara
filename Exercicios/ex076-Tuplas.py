a8eb6f068a319d16208beb558056473c6e523646
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-02 15:49:21 -0300

    Update ex076-Tuplas.py

--- Exercicios/ex076-Tuplas.py
+++ Exercicios/ex076-Tuplas.py
@@ -1,11 +1,11 @@
 produtos = 'Mangá', 9.99, 'Livro', 23.50, 'Teclado', 234.99, 'Mouse', 99.99, 'Mousepad', 34.50
 print('-'*50)
-print('LISTAGEM DE PRODUTOS')
+print(f'{"LISTAGEM DE PRODUTOS":^50}')
 print('-'*50)
 for produto in range (0, len(produtos)):
     if produto % 2 == 0:
         print(f'{produtos[produto]:.<30}',end= '')
     else:
-        print(f'R${produtos[produto]:>8}')
+        print(f'R${produtos[produto]:>8.2f}')
 print('-'*50)
 
