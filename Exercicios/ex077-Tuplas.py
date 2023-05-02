28a3d037c6369682fbddef9ad79dcf22efed21f9
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-02 15:59:34 -0300

    Update ex077-Tuplas.py

--- Exercicios/ex077-Tuplas.py
+++ Exercicios/ex077-Tuplas.py
@@ -1,6 +1,6 @@
 palavras = 'Abraço', 'Jorge', 'Marfim', 'Corinthians', 'Campeao', 'Alagoas'
 for p in palavras:
-    print(f'\nNa palavra {p} temos as vogais: ', end='')
+    print(f'\nNa palavra {p.upper()} temos as vogais: ', end='')
     for letra in p:
         if letra.lower() in 'aeiou':
             print(f'{letra}', end='')
