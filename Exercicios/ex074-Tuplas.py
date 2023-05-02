cfb1b8beed3cdeaadc61e994c6999c260559aae1
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-02 15:20:24 -0300

    Update ex074-Tuplas.py

--- Exercicios/ex074-Tuplas.py
+++ Exercicios/ex074-Tuplas.py
@@ -1,7 +1,6 @@
 from random import sample
-tupla = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+tupla = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 sorteio = sample(tupla, 5)
-
-print(sorteio)
+print(sorted(sorteio))
 print(f'O menor valor foi de {min(sorteio)}')
 print(f'O maior valor foi de {max(sorteio)}')
