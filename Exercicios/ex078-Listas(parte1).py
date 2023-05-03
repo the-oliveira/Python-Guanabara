badb0cd467afc6e01b78897b677ea82f4c116fce
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-03 19:02:03 -0300

    Update ex078-Listas(parte1).py

--- Exercicios/ex078-Listas(parte1).py
+++ Exercicios/ex078-Listas(parte1).py
@@ -1,5 +1,11 @@
 lista = []
 for valor in range(0, 5):
     lista.append(int(input('Digite um valor: ')))
-print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista))+1}')
-print(f'O menor valor digitado foi {min(lista)} na posição {lista.index(min(lista))+1}')
+if lista.count(max(lista)) >= 2:
+    print(f'O maior valor digitado foi {max(lista)} nas posições {lista.index(max(lista))+1} e {lista.index(max(lista))+1}')
+else:
+    print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista)) + 1}')
+if lista.count(min(lista)) >= 2:
+    print(f'O menor valor digitado foi {min(lista)} nas posições {lista.index(min(lista))+1} e {lista.index(min(lista))+1}')
+else:
+    print(f'O menor valor digitado foi {min(lista)} na posição {lista.index(min(lista)) + 1}')
