f291cd553654421cb87d484a6527d8dd02dc4e90
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-03 11:52:07 -0300

    Create listas.py

---
+++ Aula 13 - Listas (parte 1)/listas.py
@@ -0,0 +1,20 @@
+#listas são entre []
+#listas podem ser alteradas
+#variavel.append('valor') para adicionar algum valor no final da lista
+#variavel.insert(posição,'valor') para inserir um valor em uma posição especifica na lista
+
+
+#deletar valores na liista:
+#del variavel[posição]
+#variavel.pop(posição) = normalmente usado para deletar o ultimo valor mas pode escolher a posição entre parenteses
+#variavel.remove('valor') = para deletar um valor especifico da lista
+
+#Também da para criar listas utilizando a declaração:
+#variavel = list(range(posição inicial, posição final)), ele irá criar uma lista com os valores dentro da range
+
+#variavel.sort() ordena a lista
+#variavel.sort(reverse=True) para ordenar do maior pro menor
+
+#len(variavel) para saber o tamanho da lista
+
+
