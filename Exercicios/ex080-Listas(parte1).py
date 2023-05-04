cbdfba71a0922707db3dec3722b1291828ac9667
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-04 18:08:20 -0300

    Update ex080-Listas(parte1).py

--- Exercicios/ex080-Listas(parte1).py
+++ Exercicios/ex080-Listas(parte1).py
@@ -1,5 +1,17 @@
 lista = []
 for n in range(0, 5):
     num = int(input('Digite um valor: '))
-    lista.append(num)
+    if n == 0:
+        lista.append(num)
+        print(f'Primeiro número adicionado! Vamos mante-lo na posição {lista.index(num)} por enquanto!')
+    else:
+        if num >= max(lista):
+            lista.append(num)
+            print(f'Número adicionado ao final da lista.. posição {lista.index(num)}')
+        elif num <= min(lista):
+            lista.insert(0, num)
+            print(f'Número adicionado no inicio da lista.. posição {lista.index(num)}')
+        else:
+            lista.append(num)
+            print(f'Número adicionado na posição: {lista.index(num)}')
 print(sorted(lista))
