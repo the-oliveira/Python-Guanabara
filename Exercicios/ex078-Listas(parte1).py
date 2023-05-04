413248d4841c9093752638bc7c6a719450132afb
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-04 18:52:30 -0300

    Update ex078-Listas(parte1).py

--- Exercicios/ex078-Listas(parte1).py
+++ Exercicios/ex078-Listas(parte1).py
@@ -2,10 +2,17 @@ lista = []
 for valor in range(0, 5):
     lista.append(int(input('Digite um valor: ')))
 if lista.count(max(lista)) >= 2:
-    print(f'O maior valor digitado foi {max(lista)} nas posições {lista.index(max(lista))+1} e {lista.index(max(lista))+1}')
+    print(f'O maior valor digitado foi {max(lista)} nas posições: ', end='')
+    for p, v in enumerate(lista):
+        if v == max(lista):
+            print(f'{p}..', end='')
 else:
-    print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista)) + 1}')
+    print(f'O maior valor digitado foi {max(lista)} na posição {lista.index(max(lista))}')
+print()
 if lista.count(min(lista)) >= 2:
-    print(f'O menor valor digitado foi {min(lista)} nas posições {lista.index(min(lista))+1} e {lista.index(min(lista))+1}')
+    print(f'O menor valor digitado foi {min(lista)} nas posições: ', end='')
+    for p, v in enumerate(lista):
+        if v == min(lista):
+            print(f'{p}..', end='')
 else:
-    print(f'O menor valor digitado foi {min(lista)} na posição {lista.index(min(lista)) + 1}')
+    print(f'O menor valor digitado foi {min(lista)} na posição {lista.index(min(lista))}')
