99b715d4893b437c740235c2218897a4f12a0950
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-04 18:56:53 -0300

    Update ex079-Listas(parte1).py

--- Exercicios/ex079-Listas(parte1).py
+++ Exercicios/ex079-Listas(parte1).py
@@ -1,13 +1,15 @@
 lista = []
 while True:
     numero = int(input('Digite um valor: '))
-    if lista.count(numero) == 1:
+    if lista.count(numero) >= 1:
         print('Numero repetido, não irei adicionar!')
-        lista.remove(numero)
     else:
         lista.append(numero)
         print('Numero adicionado com sucesso!')
-    contiuar = str(input('Deseja adicionar mais números? [S/N]')).strip().upper()[0]
+    contiuar = str(input('Deseja adicionar mais números? [S/N] ')).strip().upper()[0]
+    while contiuar not in 'SN':
+        print('Comando inválido.. Tente novamente!')
+        contiuar = str(input('Deseja adicionar mais números? [S/N] ')).strip().upper()[0]
     if contiuar == 'N':
         break
 lista.sort()
