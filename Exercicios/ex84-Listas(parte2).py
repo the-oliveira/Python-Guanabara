a026180a249920252e76f49b72fd63d58dea5b98
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-10 19:56:23 -0300

    Update ex84-Listas(parte2).py

--- Exercicios/ex84-Listas(parte2).py
+++ Exercicios/ex84-Listas(parte2).py
@@ -1,5 +1,28 @@
-nome = []
+lista = []
+dados = []
 pesada = []
 leve = []
 total = 0
 while True:
+    dados.append(str(input('Digite o nome do paciente: ')))
+    dados.append(int(input('Digite o peso em kg: ')))
+    lista.append(dados[:])
+    if dados[1] >= 70:
+        pesada.append(dados[:])
+    else:
+        leve.append(dados[:])
+    total += 1
+    dados.clear()
+    continuar = str(input('Deseja cadastrar mais alguma pessoa? [S/N] ')).upper().split()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Comando inválido! Deseja cadastrar mais alguma pessoa? [S/N] ')).upper().split()[0]
+    if continuar == 'N':
+        break
+print(f'O total de pessoas cadastradas foi: {total}')
+print(f'As pessoas mais leves cadastradas foram: ', end=' ')
+for p in leve:
+    print(f'{p[0]} com {p[1]}kg, ', end='')
+print()
+print(f'As pessoas mais pesadas foram: ', end=' ')
+for p in pesada:
+    print(f'{p[0]} com {p[1]}kg, ',end=' ')
