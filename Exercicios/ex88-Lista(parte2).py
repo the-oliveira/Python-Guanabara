625c0b5dd835f0a5214fb3e6288b1908507a1248
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-13 16:05:15 -0300

    Update ex88-Lista(parte2).py

--- Exercicios/ex88-Lista(parte2).py
+++ Exercicios/ex88-Lista(parte2).py
@@ -1,6 +1,9 @@
 from random import randint
 from time import sleep
 lista = []
+print('='*40)
+print('     VAMOS TESTAR A SUA SORTE!     ')
+print('='*40)
 jogos = int(input('Deseja gerar quantos jogos? '))
 for x in range(0, jogos):
     while len(lista) != 6:
@@ -13,3 +16,6 @@ for x in range(0, jogos):
     print(f'Jogo número {x+1}: {lista}')
     sleep(1)
     lista.clear()
+print('='*40)
+print('               BOA SORTE!               ')
+print('='*40)
