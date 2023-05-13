0b52bfeaf9165b9d5eb8a9f3426587a7023da76e
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-13 15:52:12 -0300

    Update ex87-Lista(Parte2).py

--- Exercicios/ex87-Lista(Parte2).py
+++ Exercicios/ex87-Lista(Parte2).py
@@ -1,5 +1,5 @@
 lista = [[], [], []]
-par = terceira = maior = 0
+par = somacoluna = maior = 0
 for l in range(0,3):
     for c in range(0,3):
         lista[l].append(int(input(f'Digite o valor para a posição {l}.{c} ')))
@@ -8,9 +8,14 @@ for l in range(0,3):
         print(f'[ {lista[l][c]} ]', end=' ')
         if lista[l][c] % 2 == 0:
             par += lista[l][c]
-        if maior == 0:
-            maior += lista[l][c]
-        elif lista[l][c] > maior:
-            maior += lista[l][c]
     print()
 print(f'A soma dos números pares foi: {par}')
+for c in range(0,3):
+    if maior == 0:
+        maior = lista[1][c]
+    elif lista[1][c] > maior:
+        maior = lista[1][c]
+print(f'O maior número na segunda linha é {maior}')
+for l in range(0,3):
+    somacoluna += lista[l][2]
+print(f'A soma da terceira coluna é igual a: {somacoluna}')
