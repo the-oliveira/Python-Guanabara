bf05044785e068b125709ed9ace9c6ba93657d1b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-13 01:44:39 -0300

    Update ex86-Lista(parte2).py

--- Exercicios/ex86-Lista(parte2).py
+++ Exercicios/ex86-Lista(parte2).py
@@ -2,6 +2,8 @@ lista = [[], [], []]
 for l in range(0,3):
     for c in range(0,3):
         lista[l].append(int(input(f'Digite o valor para a posição {l}.{c} ')))
-print(lista[0])
-print(lista[1])
-print(lista[2])
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'{lista[l][c]}', end=' ')
+    print()
+
