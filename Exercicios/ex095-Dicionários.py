614e5fd2a70734c753ceb53104ce4d8834928c43
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-18 18:31:35 -0300

    Update ex095-Dicionários.py

--- Exercicios/ex095-Dicionários.py
+++ Exercicios/ex095-Dicionários.py
@@ -1,23 +1,20 @@
 dados = []
-lista = []
-totalgols = 0
-jogador = {'nome':'', 'partidas':''}
-while True:
-    jogador['nome'] = str(input('Qual o nome do jogador? '))
-    partidas = int(input(f'Quantas partidas {jogador["nome"]} disputou? '))
-    jogador['partidas'] = partidas
-    for p in range(0, partidas):
-        gols = int(input(f'Quantos gols na partida {p+1}? '))
-        totalgols += gols
-        jogador['total'] = totalgols
-    lista.append(jogador.copy())
-    dados.clear()
-    continuar = str(input('Quer Continuar? [S/N] ')).upper().split()[0]
-    while continuar not in "SN":
-        continuar = str(input('Quer Continuar? [S/N] ')).upper().split()[0]
-    if continuar == 'N':
-        break
-print(lista)
-for p, g in lista:
-
-
+jogador = {}
+jogador['Nome'] = str(input('Qual o nome do jogador? '))
+partidas = int(input(f'Quantas partidas {jogador["Nome"]} disputou? '))
+jogador['Partidas'] = partidas
+for p in range(0, partidas):
+    dados.append(int(input(f'Quantos gols na partida {p+1}? ')))
+jogador['Gols'] = dados[:]
+jogador['Total'] = sum(dados)
+print('=' * 50)
+print(jogador)
+print('=' * 50)
+print(f'O jogador {jogador["Nome"]} disputou {jogador["Partidas"]} partidas, marcando: ')
+for g in range(0, partidas):
+    print(f'   - Na partida {g+1} marcou {jogador["Gols"][g]} gols')
+print(f'Ao todo foram {jogador["Total"]} gols até o momento!')
+print('=' * 50)
+for k, v in jogador.items():
+    print(f' - {k}: {v}')
+print('=' * 50)
