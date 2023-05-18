4384dd9581f26a1b0a40836a1b95e7492cf4e2a8
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-18 17:45:41 -0300

    Update ex090-Dicionarios.py

--- Exercicios/ex090-Dicionarios.py
+++ Exercicios/ex090-Dicionarios.py
@@ -1,6 +1,8 @@
 alunos = {'Nome':str(input("Nome do Aluno: ")), 'Média':float(input("Média do Aluno: "))}
 if alunos['Média'] >= 7:
     alunos['Situação'] = 'Aprovado'
+elif alunos['Média'] > 5 < 7:
+    alunos['Situação'] = 'Recuperação'
 else:
     alunos['Situação'] = 'Reprovado'
 for k, v in alunos.items():
