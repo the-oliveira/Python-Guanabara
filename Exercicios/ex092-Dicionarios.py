21be90d1d9eed6ca22cd12b5318fa484bd74b1f0
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-18 18:09:51 -0300

    Update ex092-Dicionarios.py

--- Exercicios/ex092-Dicionarios.py
+++ Exercicios/ex092-Dicionarios.py
@@ -1,17 +1,16 @@
 from datetime import datetime
 dados = {}
-dados['nome'] = str(input('Digite seu nome: '))
+dados['Nome'] = str(input('Digite seu nome: '))
 nascimento = int(input('Ano de nascimento: '))
 nascimento = datetime.today().year - nascimento
-dados['idade'] = nascimento
+dados['Idade'] = nascimento
 ctps = int(input('Digite o número da sua carteira de trabalho: (000 se não houver)'))
 if ctps != 000:
-    dados['ctps'] = ctps
-    dados['salário'] = float(input('Digite o seu salário atual ou ultimo salário: R$'))
+    dados['CTPS'] = ctps
+    dados['Salário'] = float(input('Digite o seu salário atual ou ultimo salário: R$'))
     dados['Contratação'] = int(input('Ano de contratação: '))
-    dados['aposentadoria'] = dados['Contratação'] + 32
+    dados['Aposentadoria'] = nascimento + 32
 else:
-    print('Talvez seja o momento de tirar sua carteira!')
-print(dados)
+    dados['Situação'] = 'Não tem CTPS'
 for k, v in dados.items():
-    print(f'{k} {v}')
+    print(f'   - {k}: {v}')
