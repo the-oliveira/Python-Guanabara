57faa8f747f505d0449233198c3879551fb48015
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-19 16:30:03 -0300

    Update ex095-Dicionários.py

--- Exercicios/ex095-Dicionários.py
+++ Exercicios/ex095-Dicionários.py
@@ -1,20 +1,36 @@
 dados = []
 jogador = {}
-jogador['Nome'] = str(input('Qual o nome do jogador? '))
-partidas = int(input(f'Quantas partidas {jogador["Nome"]} disputou? '))
-jogador['Partidas'] = partidas
-for p in range(0, partidas):
-    dados.append(int(input(f'Quantos gols na partida {p+1}? ')))
-jogador['Gols'] = dados[:]
-jogador['Total'] = sum(dados)
-print('=' * 50)
-print(jogador)
-print('=' * 50)
-print(f'O jogador {jogador["Nome"]} disputou {jogador["Partidas"]} partidas, marcando: ')
-for g in range(0, partidas):
-    print(f'   - Na partida {g+1} marcou {jogador["Gols"][g]} gols')
-print(f'Ao todo foram {jogador["Total"]} gols até o momento!')
-print('=' * 50)
-for k, v in jogador.items():
-    print(f' - {k}: {v}')
-print('=' * 50)
+lista = []
+while True:
+    jogador['Nome'] = str(input('Qual o nome do jogador? '))
+    partidas = int(input(f'Quantas partidas {jogador["Nome"]} disputou? '))
+    jogador['Partidas'] = partidas
+    for p in range(0, partidas):
+        dados.append(int(input(f'Quantos gols na partida {p+1}? ')))
+    jogador['Gols'] = dados[:]
+    jogador['Total'] = sum(dados)
+    lista.append(jogador.copy())
+    jogador.clear()
+    continuar = str(input('Deseja continuar? [S/N] ')).upper()[0]
+    while continuar not in 'SN':
+        continuar = str(input('Comando inválido. Deseja continuar? [S/N] ')).upper()[0]
+    if continuar == 'N':
+        break
+for n, p in enumerate(lista):
+    for k, v in p.items():
+        print(f'{k} ', end=' ')
+    print()
+    for k, v in p.items():
+        print(f'{v} ', end=' ')
+    print()
+while True:
+    info = int(input('Digite um jogador que queira ver os dados: (999 para encerrar): '))
+    if info == 999:
+        break
+    else:
+        print(f'Informações do jogador {lista[info]["Nome"]}')
+        for j, p in enumerate(lista):
+            print(f'  - No jogo {j+1} fez {lista[info]["Gols"][j]}')
+print('Fim do programa')
+
+
