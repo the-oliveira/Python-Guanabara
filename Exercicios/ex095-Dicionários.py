f0dede00ea5972f583857c6714cc173e41db7bd5
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-19 16:36:25 -0300

    Update ex095-Dicionários.py

--- Exercicios/ex095-Dicionários.py
+++ Exercicios/ex095-Dicionários.py
@@ -11,17 +11,16 @@ while True:
     jogador['Total'] = sum(dados)
     lista.append(jogador.copy())
     jogador.clear()
+    dados.clear()
     continuar = str(input('Deseja continuar? [S/N] ')).upper()[0]
     while continuar not in 'SN':
         continuar = str(input('Comando inválido. Deseja continuar? [S/N] ')).upper()[0]
     if continuar == 'N':
         break
-for n, p in enumerate(lista):
-    for k, v in p.items():
-        print(f'{k} ', end=' ')
-    print()
-    for k, v in p.items():
-        print(f'{v} ', end=' ')
+for k, v in enumerate(lista):
+    print(f'{k:>3} ', end=' ')
+    for d in v.values():
+        print(f'{str(d):<15} ', end=' ')
     print()
 while True:
     info = int(input('Digite um jogador que queira ver os dados: (999 para encerrar): '))
