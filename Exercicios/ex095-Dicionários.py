0151639d58fcc24df03f8ed6ec85076be98a7404
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-19 16:50:24 -0300

    Update ex095-Dicionários.py

--- Exercicios/ex095-Dicionários.py
+++ Exercicios/ex095-Dicionários.py
@@ -2,6 +2,7 @@ dados = []
 jogador = {}
 lista = []
 while True:
+    jogador.clear()
     jogador['Nome'] = str(input('Qual o nome do jogador? '))
     partidas = int(input(f'Quantas partidas {jogador["Nome"]} disputou? '))
     jogador['Partidas'] = partidas
@@ -10,26 +11,32 @@ while True:
     jogador['Gols'] = dados[:]
     jogador['Total'] = sum(dados)
     lista.append(jogador.copy())
-    jogador.clear()
     dados.clear()
     continuar = str(input('Deseja continuar? [S/N] ')).upper()[0]
     while continuar not in 'SN':
         continuar = str(input('Comando inválido. Deseja continuar? [S/N] ')).upper()[0]
     if continuar == 'N':
         break
+print('Cod ', end=' ')
+for i in jogador.keys():
+    print(f'{i:<15}', end=' ')
+print()
 for k, v in enumerate(lista):
-    print(f'{k:>3} ', end=' ')
+    print(f'{k:>4} ', end=' ')
     for d in v.values():
-        print(f'{str(d):<15} ', end=' ')
+        print(f'{str(d):<13} ', end=' ')
     print()
 while True:
     info = int(input('Digite um jogador que queira ver os dados: (999 para encerrar): '))
     if info == 999:
         break
+    if info >= len(lista):
+        print(f'Jogador não encontrado, tente novamente!')
+        info = int(input('Digite um jogador que queira ver os dados: (999 para encerrar): '))
     else:
         print(f'Informações do jogador {lista[info]["Nome"]}')
-        for j, p in enumerate(lista):
-            print(f'  - No jogo {j+1} fez {lista[info]["Gols"][j]}')
+        for i, g in enumerate(lista[info]["Gols"]):
+            print(f'  - No jogo {i+1} fez {g}')
 print('Fim do programa')
 
 
