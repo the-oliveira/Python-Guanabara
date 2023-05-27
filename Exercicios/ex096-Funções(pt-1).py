51560f539b4b9ffab9182a4c26510915c9766cbf
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-27 14:35:01 -0300

    Update ex096-Funções(pt-1).py

--- Exercicios/ex096-Funções(pt-1).py
+++ Exercicios/ex096-Funções(pt-1).py
@@ -1,9 +1,10 @@
-def area(l, a):
-    print('='*30)
-    print(f'{"Calculando Terrenos!":^30}')
-    print('=' * 30)
-    area = l * a
-    print(f'O terreno {l} x {a}m possui {area}m² de área!')
+def area(l, c):
+    print()
+    area = l * c
+    print(f'O terreno {l} x {c}m possui {area}m² de área!')
 
 
-print(area(l=float(input('LARGURA (m)')), a= float(input('Comprimento (m): '))))
+print('='*30)
+print(f'{"Calculando Terrenos!":^30}')
+print('=' * 30)
+area(l=float(input('LARGURA (m): ')), c=float(input('Comprimento (m): ')))
