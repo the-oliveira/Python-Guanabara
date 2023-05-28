5591deb4a6313a2cdee5149df58d409892d0174e
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-28 18:58:54 -0300

    Update ex099-Funções(pt-1).py

--- Exercicios/ex099-Funções(pt-1).py
+++ Exercicios/ex099-Funções(pt-1).py
@@ -1,13 +1,15 @@
 def maior(*num):
+    print('Vamos analisar os números informados!')
+    for n in num:
+        print(f'{n} ', end=' ')
+    print()
     if len(num) == 0:
-        print(f'O maior número da lista é 0 e foram digitados 0 numeros.')
-        print()
-    print('=' * 60)
-    print(f'O maior número da lista é {max(num)} e foram digitados {len(num)} numeros.')
-    print('=' * 60)
-
+        print('Foram informado 0 números, portanto não possuí maior valor.')
+    else:
+        print(f'O maior número da lista é {max(num)} e foram digitados {len(num)} numeros.')
+    print()
 
 
 maior(2,3,32,2,1,1,2,3,4,4,4,1,1,2,2)
-maior(3, 4, 5, 6, 1,2, 0, 3012930219031, 1)
+maior(3, 4, 5, 6, 1,2, 0, 39, 1)
 maior()
