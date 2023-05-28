98da872b37c5b477ba761a68cd308aff03857d23
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-28 18:53:00 -0300

    Update ex099-Funções(pt-1).py

--- Exercicios/ex099-Funções(pt-1).py
+++ Exercicios/ex099-Funções(pt-1).py
@@ -1,9 +1,13 @@
-def maior():
-    numeros = [10, 30, 20, 11, 0, 2, 1, 2, 200, 10, 10]
-    numeros.sort()
+def maior(*num):
+    if len(num) == 0:
+        print(f'O maior número da lista é 0 e foram digitados 0 numeros.')
+        print()
     print('=' * 60)
-    print(f'O maior número da lista é {numeros[-1]} e foram digitados {len(numeros)} numeros.')
+    print(f'O maior número da lista é {max(num)} e foram digitados {len(num)} numeros.')
     print('=' * 60)
 
 
+
+maior(2,3,32,2,1,1,2,3,4,4,4,1,1,2,2)
+maior(3, 4, 5, 6, 1,2, 0, 3012930219031, 1)
 maior()
