9c7f4516fa06198bbed001601cebef47a839186d
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-28 19:21:18 -0300

    Update ex100-Funções(pt-1).py

--- Exercicios/ex100-Funções(pt-1).py
+++ Exercicios/ex100-Funções(pt-1).py
@@ -1,16 +1,30 @@
 from random import randint
-sort = [randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), ]
-numeros = [sort[:]]
-soma = 0
-
-def sorteio():
-    print(f' Os números sorteados foram: ', end=' ')
+from time import sleep
+numeros = []
+def sorteio(lista):
+    for cont in range(0, 5):
+        n = randint(1, 10)
+        numeros.append(n)
+    print(f'Os números sorteados foram: ')
     for n in numeros:
+        sleep(0.5)
         print(f'{n}', end=' ')
+    print()
 
-def somapares():
+def somapar():
+    print('Vamos somar os números pares sorteados!')
+    soma = 0
+    print('São eles: ', end=' ')
+    for n in numeros:
+        if n % 2 == 0:
+            sleep(0.5)
+            print(f'{n} ', end=' ')
+            soma += n
+    print()
+    print(f'A soma dos números pares foi igual a: {soma}')
 
 
+sorteio(numeros)
+print()
+somapar()
 
-sorteio()
-somapares()
