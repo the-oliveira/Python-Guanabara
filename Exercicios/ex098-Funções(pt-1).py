bae0f42dc17e7e1ca80101a7f8bcd9d4ad1fb47c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-28 18:27:51 -0300

    Update ex098-Funções(pt-1).py

--- Exercicios/ex098-Funções(pt-1).py
+++ Exercicios/ex098-Funções(pt-1).py
@@ -1,12 +1,23 @@
 def contador(inicio, fim, passo):
+    print('=' * 40)
     print(f'Vamos contar de {inicio} até {fim} de {passo} em {passo}')
-    cont = inicio
-    while cont <= fim:
-        print(f'{cont}', end=' ')
-        cont += passo
-    print()
+    print('=' * 40)
+    if inicio < fim:
+        cont = inicio
+        while cont <= fim:
+            print(f'{cont}', end=' ')
+            cont += passo
+        print()
+    else:
+        cont = inicio
+        while cont >= fim:
+            print(f'{cont}', end=' ')
+            cont -= passo
+        print()
 
 
 contador(1, 10, 1)
+print()
 contador(10, 0, 2)
+print()
 contador(inicio=int(input('Inicio: ')), fim=int(input('Até que número? ')), passo=int(input('De quanto em quanto? ')))
