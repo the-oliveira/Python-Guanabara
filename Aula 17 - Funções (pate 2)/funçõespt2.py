dbcfe82328022d6eaaec1e86efb3c44621b25360
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-29 16:18:28 -0300

    Update funçõespt2.py

---
+++ Aula 17 - Funções (pate 2)/funçõespt2.py
@@ -0,0 +1,19 @@
+# help() função que puxa um manual de ajuda para varias funcionalidades do python e dos modulos, é uma ajuda interativa
+# docstrings = são informações sobre uma função que você criou, é fácil de definir, basta abrir 3 aspas duplas """ """
+def contador(i, f, p):
+    """
+
+    :param i: Define o ponto de partida da contagem.
+    :param f: Define o final da contagem.
+    :param p: Define de quantos em quantos números ele irá contar.
+    :return: Sem retorno
+    """
+    c = i
+    while c <= f:
+        print(f'{c} ', end=' ')
+        c += p
+    print('fim!')
+
+
+contador(1,20,1)
+help(contador)
