e1720194282821088ffc75e459e68cb2ed3e2e2b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-31 16:08:36 -0300

    Update ex102-Funções(pt-2).py

---
+++ Exercicios/ex102-Funções(pt-2).py
@@ -0,0 +1,10 @@
+def fatorial(n=1):
+    show = 
+    for n in range(num, 0, -1):
+        if show == True:
+            print(f'{n} x', end=' ')
+        elif show == False:
+
+
+num = int(input('Digite um número: '))
+fatorial(num)
