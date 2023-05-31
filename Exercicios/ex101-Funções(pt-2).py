b3263307f75628cc5804f514a8826e7988434dce
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-05-31 15:48:42 -0300

    Create ex101-Funções(pt-2).py

---
+++ Exercicios/ex101-Funções(pt-2).py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+
+def voto(nascimento):
+    n = datetime.today().year - nascimento
+    if n < 16:
+        return 'NÃO PODE VOTAR'
+    elif n >= 18 and n < 68:
+        return 'VOTO OBRIGATÓRIO'
+    elif n > 16 and n < 18 and n > 68:
+        return 'VOTO OPCIONAL'
+
+
+idade = int(input('Digite o ano de seu nascimento: '))
+print(f'Com {datetime.today().year - idade} anos {voto(idade)}')
