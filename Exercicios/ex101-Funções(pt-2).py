6b1271e7645ec545cabf9c12c5e33dbb8b3476f0
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-04 16:00:15 -0300

    Update ex101-Funções(pt-2).py

--- Exercicios/ex101-Funções(pt-2).py
+++ Exercicios/ex101-Funções(pt-2).py
@@ -7,11 +7,11 @@ def voto(nascimento):
     from datetime import datetime
     n = datetime.today().year - nascimento
     if n < 16:
-        return print(f'Com {n} anos: NÃO PODE VOTAR!')
+        return f'Com {n} anos: NÃO PODE VOTAR!'
     elif n >= 18 and n < 68:
-        return print(f'Com {n} anos: O VOTO É OBRIGATÓRIO!')
-    elif n >= 16 and n <= 17 or n > 68:
-        return print(f'Com {n} anos: O VOTO É OPCIONAL!')
+        return f'Com {n} anos: O VOTO É OBRIGATÓRIO!'
+    elif n >= 16 and n <= 17 or n > 65:
+        return f'Com {n} anos: O VOTO É OPCIONAL!'
 
 
 idade = int(input('Digite o ano de seu nascimento: '))
