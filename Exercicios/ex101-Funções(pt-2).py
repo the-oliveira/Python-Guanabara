ee363f5b18e632ca1f41c0f74e17b1d3e301cdcf
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-04 15:57:43 -0300

    Update ex101-Funções(pt-2).py

--- Exercicios/ex101-Funções(pt-2).py
+++ Exercicios/ex101-Funções(pt-2).py
@@ -7,7 +7,7 @@ def voto(nascimento):
         return print(f'Com {n} anos: NÃO PODE VOTAR!')
     elif n >= 18 and n < 68:
         return print(f'Com {n} anos: O VOTO É OBRIGATÓRIO!')
-    elif n > 16 and n < 18 or n > 68:
+    elif n >= 16 and n <= 17 or n > 68:
         return print(f'Com {n} anos: O VOTO É OPCIONAL!')
 
 
