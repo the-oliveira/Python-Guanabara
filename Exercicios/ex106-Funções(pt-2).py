96b9db7da539aaf33ef1422048acfa4fe12aa709
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-06 16:37:27 -0300

    Update ex106-Funções(pt-2).py

--- Exercicios/ex106-Funções(pt-2).py
+++ Exercicios/ex106-Funções(pt-2).py
@@ -2,10 +2,18 @@ from time import sleep
 cores = ('\033[m',             #limpa    0
          '\033[1;34;40m',      #Azul     1
          '\033[1;36;40m',      #Ciano    2
-         '\033[1:30:41m'       #Vermelho 3
+         '\033[1:30:41m',      #Vermelho 3
+         '\033[1:35m'          #Magenta  4
          )
+
+
 def Pyhelp(comando):
+    titulo(f'ACESSANDO O MANUAL \'{comando}\'', 1)
+    sleep(2)
+    print(cores[4], end='')
     help(comando)
+    print(cores[0], end='')
+
 
 def titulo(msg, cor):
     tamanho = len(msg) + 4
@@ -14,6 +22,7 @@ def titulo(msg, cor):
     print(f'  {msg}')
     print('=' * tamanho)
     print(cores[0], end='')
+    sleep(1)
 
 while True:
 
@@ -21,9 +30,7 @@ while True:
     comando = str(input('\033[1:32:40m[PyHELP]   DIGITE UM COMANDO OU BIBLIOTECA: '))
     if comando.lower() == 'fim':
         titulo('[PyHELP] Fim do programa! Volte sempre!', 2)
-        sleep(1)
         break
     else:
         Pyhelp(comando)
-    sleep(0.5)
 
