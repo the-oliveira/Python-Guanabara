c0f6743f3939bfd86f86348dede1a683d4dbb08b
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-06 16:30:49 -0300

    Update ex106-Funções(pt-2).py

--- Exercicios/ex106-Funções(pt-2).py
+++ Exercicios/ex106-Funções(pt-2).py
@@ -1,16 +1,29 @@
-def Pyhelp():
-    from time import sleep
-    while True:
-        print('\033[1:30:45m=' * 30)
-        print(f'\033[1:30:45m    SISTEMA DE AJUDA PYHELP   ')
-        print('\033[1:30:45m=' * 30)
-        comando = str(input('\033[1:32:40m[PyHELP]   DIGITE UM COMANDO OU BIBLIOTECA: '))
-        if comando.lower() == 'fim':
-            print('\033[1:30:42m[PyHELP] Fim do programa! Volte sempre!')
-            sleep(1)
-            break
-        sleep(0.5)
-        help(comando)
+from time import sleep
+cores = ('\033[m',             #limpa    0
+         '\033[1;34;40m',      #Azul     1
+         '\033[1;36;40m',      #Ciano    2
+         '\033[1:30:41m'       #Vermelho 3
+         )
+def Pyhelp(comando):
+    help(comando)
 
+def titulo(msg, cor):
+    tamanho = len(msg) + 4
+    print(cores[cor], end='')
+    print('=' * tamanho)
+    print(f'  {msg}')
+    print('=' * tamanho)
+    print(cores[0], end='')
 
-Pyhelp()
+while True:
+
+    titulo('SISTEMA DE AJUDA PYHELP', 3)
+    comando = str(input('\033[1:32:40m[PyHELP]   DIGITE UM COMANDO OU BIBLIOTECA: '))
+    if comando.lower() == 'fim':
+        titulo('[PyHELP] Fim do programa! Volte sempre!', 2)
+        sleep(1)
+        break
+    else:
+        Pyhelp(comando)
+    sleep(0.5)
+
