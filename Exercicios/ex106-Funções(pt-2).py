b3d0be7f56eca05736fb89c1d27da3afba7b73f6
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-06 16:12:28 -0300

    Update ex106-Funções(pt-2).py

--- Exercicios/ex106-Funções(pt-2).py
+++ Exercicios/ex106-Funções(pt-2).py
@@ -1,10 +1,16 @@
 def Pyhelp():
-    print('\033[1:30:41m=\033[m' * 30)
-    print(f'\033[1:30:41m    SISTEMA DE AJUDA PYHELP   \033[m')
-    print('\033[1:30:41m=\033[m' * 30)
-    comando = input('\033[1:47m[PyHELP]   DIGITE UM COMANDO: \033[1:43m')
-    while comando not in 'fim':
-        return help(comando)
-        comando = input('\033[1:47m[PyHELP]   DIGITE UM COMANDO: \033[1:43m')
+    from time import sleep
+    while True:
+        print('\033[1:30:45m=' * 30)
+        print(f'\033[1:30:45m    SISTEMA DE AJUDA PYHELP   ')
+        print('\033[1:30:45m=' * 30)
+        comando = str(input('\033[1:32:40m[PyHELP]   DIGITE UM COMANDO OU BIBLIOTECA: '))
+        if comando == 'finalizar':
+            print('\033[1:30:45m Fim do programa! Volte sempre!')
+            sleep(1)
+            break
+        sleep(0.5)
+        help(comando)
+
 
 Pyhelp()
