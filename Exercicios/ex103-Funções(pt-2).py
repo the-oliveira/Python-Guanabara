bea579acc4b59e33f75adc64b3b470a3961386e6
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-06 15:21:12 -0300

    Update ex103-Funções(pt-2).py

--- Exercicios/ex103-Funções(pt-2).py
+++ Exercicios/ex103-Funções(pt-2).py
@@ -1,4 +1,4 @@
-def ficha(j='<Desconhecido>', g=0, a=0):
+def ficha(jog='<Desconhecido>', g=0, a=0):
     """
     Programa que retorna os dados de um jogador.
     :param j: String com o nome do jogador
@@ -8,16 +8,21 @@ def ficha(j='<Desconhecido>', g=0, a=0):
     """
     j = str(input('Nome do jogador: '))
     if j.strip() == '':
-        j = '<Desconhecido>'
-    g = str(input('Quantidade de gols: '))
-    if g.isnumeric() == False:
-        g = 0
-    a = str(input('Quantidade de assistências: '))
-    if a.isnumeric() == False:
-        a = 0
+        j = jog
+    gols = str(input('Quantidade de gols: '))
+    if gols.isnumeric() == False:
+        gols = g
+    else:
+        g = gols
+    assist = str(input('Quantidade de assistências: '))
+    if assist.isnumeric() == False:
+        assist = a
+    else:
+        a = assist
     return print(f'O jogador {j} fez {g} gols e {a} assistências no campeonato!')
 
 
 print('=' * 60)
 ficha()
 print('=' * 60)
+help(ficha)
