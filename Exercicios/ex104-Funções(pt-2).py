08133d7abe598b4bc4ffc482f61cc6eb8fccaee5
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-06 15:34:45 -0300

    Update ex104-Funções(pt-2).py

--- Exercicios/ex104-Funções(pt-2).py
+++ Exercicios/ex104-Funções(pt-2).py
@@ -1,9 +1,15 @@
-def leiaint(n):
-    int(input(n))
-    while n not in range(-99999, 999999999):
-        print('Comando inválido! tente novamente.')
-        n = leiaint('Digite um número: ')
-
+def leiaint(msg):
+    valido = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = n
+            valido = True
+            break
+        else:
+            print(f'\033[1:31mErro, digite um número inteiro válido: \033[m')
+    return valor
 
 
 n = leiaint('Digite um número: ')
