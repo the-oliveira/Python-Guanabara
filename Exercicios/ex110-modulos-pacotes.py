39dc2ce9ef2ee76329dfa3aff1e27f347a0158a0
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-13 18:29:32 -0300

    Resolução ex 110

---
+++ Exercicios/ex110-modulos-pacotes.py
@@ -0,0 +1,4 @@
+import moeda
+#Exercicio tornando o código menor, acrescentando a função resumo no init
+p = float(input('Digite um preço: R$'))
+moeda.resumo(p, 15, 20)
--- Exercicios/moeda/__init__.py
+++ Exercicios/moeda/__init__.py
@@ -20,3 +20,15 @@ def desconta(valor=0, desconto=0, formato=False):
 
 def moeda(valor=0, moeda='R$', formato=False):
     return f'{moeda}{valor:>.2f}'.replace('.', ',')
+
+
+def resumo(valor=0, aumentando=0, descontando=0):
+    print('=' * 30)
+    print(f'{"Analisando Valores":^30}')
+    print('=' * 30)
+    print(f'Preço analisado:{moeda(valor):>12}')
+    print(f'Dobro do preço:{dobro(valor, True):>12}')
+    print(f'Metade do valor:{metade(valor, True):>12}')
+    print(f'Aumentando {aumentando}% :{aumenta(valor, aumentando, True):>12}')
+    print(f'Descontando {descontando}% :{desconta(valor, descontando, True):>12}')
+    print('=' * 30)
