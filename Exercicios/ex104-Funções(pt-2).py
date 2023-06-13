0ad6f10e53b8a8019f041c6723b0824dd7535c28
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-13 19:04:55 -0300

    finalizando o ex 112

--- Exercicios/ex104-Funções(pt-2).py
+++ Exercicios/ex104-Funções(pt-2).py
@@ -1,16 +1,3 @@
-def leiaint(msg):
-    valido = False
-    valor = 0
-    while True:
-        n = str(input(msg))
-        if n.isnumeric():
-            valor = n
-            valido = True
-            break
-        else:
-            print(f'\033[1:31mErro, digite um número inteiro válido: \033[m')
-    return valor
-
-
-n = leiaint('Digite um número: ')
+from utilidades import dados
+n = dados.leiaint('Digite um número: ')
 print(f'Você digitou o número {n}!')
--- utilidades/dados/__init__.py
+++ utilidades/dados/__init__.py
@@ -7,3 +7,17 @@ def leiadinheiro (msg):
         else:
             valido = True
             return float(entrada)
+
+
+def leiaint(msg):
+    valido = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = n
+            valido = True
+            break
+        else:
+            print(f'\033[1:31mErro, digite um número inteiro válido: \033[m')
+    return valor
