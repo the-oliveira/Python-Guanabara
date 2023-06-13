bb8316c8c90c41a1b20192423588d2c68537c988
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-13 15:41:09 -0300

    ex108

---
+++ Exercicios/ex108-modulos-pacotes.py
@@ -0,0 +1,6 @@
+import moeda
+p = float(input('Digite um preço: R$'))
+print(f'A metade de {moeda.moeda(p)} é {moeda.moeda(moeda.metade(p))}')
+print(f'O Dobro de {moeda.moeda(p)} é {moeda.moeda(moeda.dobro(p))}')
+print(f'Descontando 10% de {moeda.moeda(p)} o valor fica {moeda.moeda(moeda.aumenta(p, 10))}')
+print(f'Descontando 13% de {moeda.moeda(p)} o valor fica {moeda.moeda(moeda.desconta(p, 13))}')
--- Exercicios/moeda/__init__.py
+++ Exercicios/moeda/__init__.py
@@ -1,18 +1,22 @@
-def dobro(valor):
+def dobro(valor=0):
     res = dobro = valor * 2
     return res
 
 
-def metade(valor):
+def metade(valor=0):
     res = metade = valor / 2
     return res
 
 
-def aumenta(valor, aumento=0):
+def aumenta(valor=0, aumento=0):
     res = dez = (valor * aumento / 100) + valor
     return res
 
 
-def desconta(valor, desconto=0):
+def desconta(valor=0, desconto=0):
     res = treze = valor - (valor * desconto / 100)
     return res
+
+
+def moeda(valor=0, moeda='R$'):
+    return f'{moeda}{valor:.2f}'.replace('.', ',')
