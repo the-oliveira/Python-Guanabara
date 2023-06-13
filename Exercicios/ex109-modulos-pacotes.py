26039bf939b1e88540b2c6c02fa64709edafe25c
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-13 18:14:19 -0300

    resolvendo exercicios

---
+++ Exercicios/ex109-modulos-pacotes.py
@@ -0,0 +1,8 @@
+import moeda
+#Exercicio simplificando a chamada da conversão de moeda, para isso utilizamos um if antes de dar return no 'res'
+#Adicionamos também a condicional True para formato
+p = float(input('Digite um preço: R$'))
+print(f'A metade de {moeda.moeda(p)} é {moeda.metade(p, True)}')
+print(f'O Dobro de {moeda.moeda(p)} é {moeda.dobro(p, True)}')
+print(f'Descontando 10% de {moeda.moeda(p)} o valor fica {moeda.aumenta(p, 10, True)}')
+print(f'Descontando 13% de {moeda.moeda(p)} o valor fica {moeda.desconta(p, 13, True)}')
--- Exercicios/moeda/__init__.py
+++ Exercicios/moeda/__init__.py
@@ -1,22 +1,22 @@
-def dobro(valor=0):
+def dobro(valor=0, formato=False):
     res = dobro = valor * 2
-    return res
+    return res if formato == False else moeda(res)
 
 
-def metade(valor=0):
+def metade(valor=0, formato=False):
     res = metade = valor / 2
-    return res
+    return res if formato == False else moeda(res)
 
 
-def aumenta(valor=0, aumento=0):
+def aumenta(valor=0, aumento=0, formato=False):
     res = dez = (valor * aumento / 100) + valor
-    return res
+    return res if formato == False else moeda(res)
 
 
-def desconta(valor=0, desconto=0):
+def desconta(valor=0, desconto=0, formato=False):
     res = treze = valor - (valor * desconto / 100)
-    return res
+    return res if formato == False else moeda(res)
 
 
-def moeda(valor=0, moeda='R$'):
+def moeda(valor=0, moeda='R$', formato=False):
     return f'{moeda}{valor:>.2f}'.replace('.', ',')
