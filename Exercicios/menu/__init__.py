71c6f15d6a0d6721f8f2771ed75872bf01333253
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-15 21:36:05 -0300

    .

--- Exercicios/menu/__init__.py
+++ Exercicios/menu/__init__.py
@@ -22,16 +22,25 @@ def menuprincipal():
                 print('=' * 40)
                 print('oi 2')
                 print('=' * 40)
-            else:
+            elif escolha == 3:
                 print('=' * 40)
                 print(f'\033[1:32m{"Programa encerrado, volte sempre!!":^40}\033[m')
                 print('=' * 40)
                 break
+            else:
+                print('=' * 40)
+                print('\033[1:31mOpção inválida!\033[m'.center(50))
+                print('=' * 40)
         except (ValueError, TypeError):
-            print(f'\033[1:31mEntrada de valor inválida, tente novamente.\033[m')
+            print('=' * 40)
+            print(f'\033[1:31mEntrada inválida!\033[m'.center(50))
+            print('=' * 40)
             continue
         except KeyboardInterrupt:
-            print(f'\033[1:31mPrograma interrompido pelo usuário.\033[m')
+            print()
+            print('=' * 40)
+            print(f'\033[1:31mPrograma interrompido pelo usuário.\033[m'.center(50))
+            print('=' * 40)
             break
 
 
