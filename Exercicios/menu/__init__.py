1e990ad26482d10be4f008b1c1d2a9d18552a306
Dudu Oliveira <dudu_olive43@hotmail.com>
2023-06-15 21:26:26 -0300

    ex115

--- Exercicios/menu/__init__.py
+++ Exercicios/menu/__init__.py
@@ -1,15 +1,27 @@
 def menuprincipal():
-    print('=' * 30)
-    print(f'{"MENU PRINCIPAL":^30}')
-    print('=' * 30)
+    """
+    Menu interativo para ver, cadastrar ou sair do sistema, feito com tratamento de erro.
+    :return: None
+    """
+    print('=' * 40)
+    print(f'\033[1:34m{"MENU PRINCIPAL":^40}\033[m')
+    print('=' * 40)
+    print('\033[1:36m1 - Ver pessoas cadastradas\033[m')
+    print('\033[1:36m2 - Cadastrar nova Pessoa\033[m')
+    print('\033[1:36m3 - Sair do Sistema\033[m')
+    print('=' * 40)
     escolha = 0
     while True:
         try:
-            escolha = int(input('Escolha uma opção: '))
+            escolha = int(input('\033[1:35mSua opção: \033[m'))
             if escolha == 1:
+                print('=' * 40)
                 print('oi')
+                print('=' * 40)
             elif escolha == 2:
+                print('=' * 40)
                 print('oi 2')
+                print('=' * 40)
             else:
                 print('=' * 40)
                 print(f'\033[1:32m{"Programa encerrado, volte sempre!!":^40}\033[m')
